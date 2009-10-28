771e68963b1ccbe779ab272d8ad1fb4e0439026f
guilherme silveira <guilherme.silveira@caelum.com.br>
2009-10-28 13:35:29 -0200

    support atom and name based links

--- lib/restfulie.rb
+++ lib/restfulie.rb
@@ -1,23 +1,23 @@
 module Restfulie  
   alias :to_json_old :to_json
   def to_json
-    to_json_old :methods => :acts
+    to_json_old :methods => :following_states
   end
   
   def to_xml(options = {})
     controller = options[:controller]
-    if controller.nil?
-      return to_xml_old
-    end
-    to_xml_old :skip_types => true do |xml|
-      acts.each do |action|
-        # direct link to action
+    return to_xml_old if controller.nil?
+    
+    use_name_based_link = options[:use_name_based_link]==true
+    options[:skip_types] = true
+    to_xml_old options do |xml|
+      following_states.each do |action|
         rel = action[:rel]
         rel = action[:action] if rel.nil?
         translate_href = controller.url_for(action)
-        # xml.tag!('atom:link', 'xpto', 'abcd')
-        xml.link :rel => rel, :href => translate_href
-      end
+        xml.tag!('atom:link', 'xmlns:atom' => 'http://www.w3.org/2005/Atom', :rel => rel, :href => translate_href) unless use_name_based_link
+        xml.tag!(rel, translate_href) if use_name_based_link
+      end if respond_to?(:following_states)
     end
   end
 end
