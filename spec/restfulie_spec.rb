d4565f15a5e27bb92b112ae1e411b7d3fabdb509
guilherme silveira <guilherme.silveira@caelum.com.br>
2009-10-29 09:57:35 -0200

    fixed json with array of actiosn: must be an array

--- spec/restfulie_spec.rb
+++ spec/restfulie_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 class RestfulieModel < ActiveRecord::Base
   def following_states
-    {:rel => "next_state"}
+    [{:rel => "next_state"}]
   end
 end
 
@@ -15,7 +15,7 @@ end
 describe RestfulieModel do
   context "when parsed to json" do
     it "should include the method following_states" do
-      subject.to_json.should eql("{\"following_states\":{\"rel\":\"next_state\"}}")
+      subject.to_json.should eql("{\"following_states\":[{\"rel\":\"next_state\"}]}")
     end
   end
 
