9f56134e0e3ab4753aabec9c5e615b6215beee20
guilherme silveira <guilherme.silveira@caelum.com.br>
2009-10-29 09:56:44 -0200

    fixed json test for display rel

--- spec/restfulie_spec.rb
+++ spec/restfulie_spec.rb
@@ -2,20 +2,30 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 class RestfulieModel < ActiveRecord::Base
   def following_states
-    "anything"
+    {:rel => "next_state"}
+  end
+end
+
+class MockedController
+  def url_for(x)
+    "http://url_for/#{x['ref']}"
   end
 end
 
 describe RestfulieModel do
   context "when parsed to json" do
     it "should include the method following_states" do
-      subject.to_json.should eql("{\"following_states\":\"#{subject.following_states}\"}")
+      subject.to_json.should eql("{\"following_states\":{\"rel\":\"next_state\"}}")
     end
   end
 
   context "when parsed to xml" do
     it "should not add hypermedia if controller is nil" do
       subject.to_xml.gsub("\n", '').should eql('<?xml version="1.0" encoding="UTF-8"?><restfulie-model></restfulie-model>')
+    end
+    it "should add hypermedia atom link if controller is set" do
+      my_controller = MockedController.new
+      subject.to_xml(:controller => my_controller).gsub("\n", '').should eql('<?xml version="1.0" encoding="UTF-8"?><restfulie-model></restfulie-model>')
     end
   end
 end
