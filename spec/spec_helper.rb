f457f072371833fa39ea6097869d44d98301b5ff
guilherme silveira <guilherme.silveira@caelum.com.br>
2009-11-23 15:13:59 -0200

    removed link to controller deleted file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,8 +8,6 @@ require 'rexml/document'
 require 'active_record'
 require 'action_controller'
 
-require 'restfulie_controller'
-
 ActiveRecord::Base.establish_connection(
   "adapter" => "sqlite3", "database" => File.join(File.dirname(__FILE__), '..', 'test.sqlite3')
 )
