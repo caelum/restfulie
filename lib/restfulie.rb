8ceb4e36759a605e8780a5b37373b3a03a2b483b
guilherme silveira <guilherme.silveira@caelum.com.br>
2009-11-23 00:22:44 -0200

    todo

--- lib/restfulie.rb
+++ lib/restfulie.rb
@@ -9,7 +9,7 @@ module Restfulie
   include Restfulie::Marshalling
   include Restfulie::Transitions
 
-  # checks if its possible to execute such transition and executes it
+  # checks if its possible to execute such transition and, if it is, executes it
   def move_to(name)
     raise "Current state #{status} is invalid in order to execute #{name}. It must be one of #{transitions}" unless available_transitions[:allow].include? name
     self.class.transitions[name].execute_at result
