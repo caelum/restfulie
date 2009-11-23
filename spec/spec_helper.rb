1e1ee851f7029710b51dab29c31134e341baf001
guilherme silveira <guilherme.silveira@caelum.com.br>
2009-11-23 15:16:37 -0200

    tests fixed due to problem on requiring files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,8 @@ require 'rexml/document'
 require 'active_record'
 require 'action_controller'
 
+require 'restfulie'
+
 ActiveRecord::Base.establish_connection(
   "adapter" => "sqlite3", "database" => File.join(File.dirname(__FILE__), '..', 'test.sqlite3')
 )
