fddf9b8998dd7c0009e43d46c968cf6bfeffe35b
guilherme silveira <guilherme.silveira@caelum.com.br>
2009-11-22 23:15:17 -0200

    docs

--- lib/restfulie.rb
+++ lib/restfulie.rb
@@ -15,9 +15,20 @@ module Restfulie
     self.class.transitions[name].execute_at result
   end
 
-  # returns a list with extra possible transitions
-  # those transitions will be concatenated with any extra transitions provided by your resource through
-  # the use of state and transition definitions  
+  # Returns an array with extra possible transitions.
+  # Those transitions will be concatenated with any extra transitions provided by your resource through
+  # the use of state and transition definitions.
+  # For every transition its name is the only mandatory field:
+  # options = {}
+  # [:show, options] # will generate a link to your controller's show action
+  #
+  # The options can be used to override restfulie's conventions:
+  # options[:rel] = "refresh" # will create a rel named refresh
+  # options[:action] = "destroy" # will link to the destroy method
+  # options[:controller] = another controller # will use another controller's action
+  #
+  # Any extra options will be passed to the target controller url_for method in order to retrieve
+  # the transition's uri.
   def following_transitions
     []
   end
