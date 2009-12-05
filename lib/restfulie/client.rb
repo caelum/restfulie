d8684d72fc33042ae2b385e6a591cdd9090787bd
guilherme silveira <guilherme.silveira@caelum.com.br>
2009-12-05 02:51:46 -0200

    docs as usual

--- lib/restfulie/client.rb
+++ lib/restfulie/client.rb
@@ -10,11 +10,16 @@ require 'restfulie/client/instance'
 require 'restfulie/client/state'
 
 module Restfulie
+  
+  # Extends your class to support restfulie-client side's code.
+  # This will extends Restfulie::Client::Base methods as class methods,
+  # Restfulie::Client::Instance as instance methods and Restfulie::Unmarshalling as class methods.
   def uses_restfulie
     extend Restfulie::Client::Base
     include Restfulie::Client::Instance
-    extend Restfulie::Unmarshalling #???
+    extend Restfulie::Unmarshalling
   end
+  
 end
 
 Object.extend Restfulie
