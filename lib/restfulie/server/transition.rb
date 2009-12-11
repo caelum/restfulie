781994dce5bcce7a1c0721c419e835b1884b9565
guilherme silveira <guilherme.silveira@caelum.com.br>
2009-12-11 20:03:24 -0200

    breaking link_for method

--- lib/restfulie/server/transition.rb
+++ lib/restfulie/server/transition.rb
@@ -26,17 +26,7 @@ module Restfulie
       def link_for(model, controller)
         specific_action = @body ? @body.call(model) : action.dup
 
-        # if you use the class level DSL, you will need to add a lambda for instance level accessors:
-        #   transition :show, {:action => :show, :foo_id => lambda { |model| model.id }}
-        # but you can replace it for a symbol and defer the model call
-        #   transition :show, {:action => :show, :foo_id => :id}
-        specific_action = specific_action.inject({}) do |actions, pair|
-          if pair.last.is_a?( Symbol ) && model.attributes.include?(pair.last)
-            actions.merge!( pair.first => model.send(pair.last) )
-          else
-            actions.merge!( pair.first => pair.last )
-          end
-        end
+        specific_action = parse_specific_action(specific_action, model)
 
         rel = specific_action[:rel] || @name
         specific_action[:rel] = nil
@@ -45,6 +35,23 @@ module Restfulie
         uri = controller.url_for(specific_action)
         
         return rel, uri
+      end
+      
+      private
+
+      # if you use the class level DSL, you will need to add a lambda for instance level accessors:
+      #   transition :show, {:action => :show, :foo_id => lambda { |model| model.id }}
+      # but you can replace it for a symbol and defer the model call
+      #   transition :show, {:action => :show, :foo_id => :id}
+      def parse_specific_action(action, model)
+        action.inject({}) do |actions, pair|
+          if pair.last.is_a?( Symbol ) && model.attributes.include?(pair.last)
+            actions.merge!( pair.first => model.send(pair.last) )
+          else
+            actions.merge!( pair.first => pair.last )
+          end
+        end
+        action
       end
       
     end
