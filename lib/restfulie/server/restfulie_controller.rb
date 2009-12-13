c4b03ea55150e792f53aff9461c0787f7c4a3eeb
guilherme silveira <guilherme.silveira@caelum.com.br>
2009-12-13 12:20:56 -0200

    extracted unless

--- lib/restfulie/server/restfulie_controller.rb
+++ lib/restfulie/server/restfulie_controller.rb
@@ -5,8 +5,10 @@ module Restfulie
     def create
       
       type = self.class.name[/(.*)Controller/,1].singularize.constantize
-      return head 415 unless Restfulie::MediaType.supports?(request.headers['CONTENT_TYPE'])
-      return head 415 unless Restfulie::MediaType.media_type(request.headers['CONTENT_TYPE']) == type
+      unless Restfulie::MediaType.supports?(request.headers['CONTENT_TYPE']) ||
+              Restfulie::MediaType.media_type(request.headers['CONTENT_TYPE']) == type
+        return head 415
+      end
       
       @model = Restfulie.from request
       if @model.save
