078fdbbd54f6b1b8a08a849e07368887a42dbca7
guilherme silveira <guilherme.silveira@caelum.com.br>
2009-12-20 12:55:42 -0200

    cutting data

--- lib/restfulie/media_type_defaults.rb
+++ lib/restfulie/media_type_defaults.rb
@@ -2,28 +2,28 @@ module Restfulie
   
   module MediaType
     def self.HtmlType
-      Restfulie::MediaType.custom_type('html', self, lambda {})
+      custom_type('html', self, lambda {})
     end
     def self.TextHtmlType
-      Restfulie::MediaType.custom_type('text/html', self, lambda {})
+      custom_type('text/html', self, lambda {})
     end
   
     # TODO rename it and move it
     def self.default_types
       [Restfulie::MediaType.HtmlType,
         Restfulie::MediaType.TextHtmlType,
-        Restfulie::MediaType.rendering_type('application/xml', self),
-        Restfulie::MediaType.rendering_type('application/json', self),
-        Restfulie::MediaType.rendering_type('xml', self),
-        Restfulie::MediaType.rendering_type('json', self)]
+        rendering_type('application/xml', self),
+        rendering_type('application/json', self),
+        rendering_type('xml', self),
+        rendering_type('json', self)]
     end
 
     # TODO should allow aliases...
-    Restfulie::MediaType.register(Restfulie::MediaType.HtmlType)
-    Restfulie::MediaType.register(Restfulie::MediaType.TextHtmlType)
-    Restfulie::MediaType.register(rendering_type('application/xml', DefaultMediaTypeDecoder))
-    Restfulie::MediaType.register(rendering_type('application/json', DefaultMediaTypeDecoder))
-    Restfulie::MediaType.register(rendering_type('xml', DefaultMediaTypeDecoder))
-    Restfulie::MediaType.register(rendering_type('json', DefaultMediaTypeDecoder))
+    register(Restfulie::MediaType.HtmlType)
+    register(Restfulie::MediaType.TextHtmlType)
+    register(rendering_type('application/xml', DefaultMediaTypeDecoder))
+    register(rendering_type('application/json', DefaultMediaTypeDecoder))
+    register(rendering_type('xml', DefaultMediaTypeDecoder))
+    register(rendering_type('json', DefaultMediaTypeDecoder))
   end
 end
