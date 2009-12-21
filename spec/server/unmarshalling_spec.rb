fdc749218284c394baa9734915a1900f2eabc3e3
guilherme silveira <guilherme.silveira@caelum.com.br>
2009-12-20 22:26:42 -0200

    fixed unmarshalling tests

--- spec/server/unmarshalling_spec.rb
+++ spec/server/unmarshalling_spec.rb
@@ -3,25 +3,6 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 context Restfulie::Server::Base do
   
   context "while registering a mime type" do
-    it "should be able to unmarshall the data" do
-      class City
-      end
-      
-      body = Object.new
-      body.should_receive(:string).and_return("my custom content")
-      
-      request = Object.new
-      request.should_receive(:body).and_return(body)
-      request.should_receive(:headers).and_return({"CONTENT_TYPE" => 'application/vnd.caelum_city+xml'})
-      
-      Restfulie::MediaType.should_receive(:media_type).with("application/vnd.caelum_city+xml").and_return(City)
-      
-      result = Object.new
-      City.should_receive(:from_xml).with("my custom content").and_return(result)
-      city = Restfulie.from request
-      city.should eql(result)
-      
-    end
   end
   
 end
