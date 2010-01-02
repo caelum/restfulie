259252813772d198fe27c1191ea6403257f36159
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-01-02 19:26:59 -0200

    removed wiki link

--- lib/restfulie/client/atom_media_type.rb
+++ lib/restfulie/client/atom_media_type.rb
@@ -1,10 +1,9 @@
-# require 'atom'
-
 module Restfulie
   
   # TODO break media type registering for DECODING and ENCODING appart, so we can have two files
   module Server
     
+    # a decoder from atom an feed
     class AtomMediaType < Restfulie::MediaType::Type
       
       def self.from_xml(xml)
@@ -14,12 +13,14 @@ module Restfulie
       
     end
     
+    # an atom feed
     class AtomFeedDecoded < Hashi::CustomHash
       
       def initialize(hash)
         super(hash)
       end
-      
+  
+      # retrieves the nth element from an atom feed
       def [](position)
         hash = entry[position].content.hash
         hash = hash.dup
