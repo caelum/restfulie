d63485237c29707e21e417b670625aa88c5143dd
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-01-07 15:05:22 -0200

    all groups of error messages supported

--- lib/restfulie/client/extensions/http.rb
+++ lib/restfulie/client/extensions/http.rb
@@ -9,6 +9,26 @@ module Restfulie
         code.to_i >= 200 && code.to_i <= 299
       end
         
+      # determines if this response code was successful (according to http specs: 100~199)
+      def is_informational?
+        code.to_i >= 100 && code.to_i <= 199
+      end
+        
+      # determines if this response code was successful (according to http specs: 300~399)
+      def is_redirection?
+        code.to_i >= 300 && code.to_i <= 399
+      end
+        
+      # determines if this response code was successful (according to http specs: 400~499)
+      def is_client_error?
+        code.to_i >= 400 && code.to_i <= 499
+      end
+        
+      # determines if this response code was successful (according to http specs: 500~599)
+      def is_server_error?
+        code.to_i >= 500 && code.to_i <= 599
+      end
+        
     end
   end
 end
--- spec/client/extensions/http_spec.rb
+++ spec/client/extensions/http_spec.rb
@@ -2,17 +2,37 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 context Restfulie::Client::HTTPResponse do
   
-  it "should return successful for 200~299" do
+  def check_complies_with(from, to, method)
     o = Hashi::CustomHash.new
     o.extend Restfulie::Client::HTTPResponse
-    (200..299).each do |code|
+    (from..to).each do |code|
       o.code = code
-      o.is_successful?.should be_true
+      o.send(method).should be_true
     end
-    [199,300].each do |code|
+    [from-1,to+1].each do |code|
       o.code = code
-      o.is_successful?.should be_false
+      o.send(method).should be_false
     end
+  end
+  
+  it "should return successful for 200~299" do
+    check_complies_with(200, 299, :is_successful?)
+  end
+
+  it "should return informational for 100~199" do
+    check_complies_with(100, 199, :is_informational?)
+  end
+
+  it "should return redirection for 300~399" do
+    check_complies_with(300, 399, :is_redirection?)
+  end
+
+  it "should return redirection for 400~499" do
+    check_complies_with(400, 499, :is_client_error?)
+  end
+
+  it "should return redirection for 500~599" do
+    check_complies_with(500, 599, :is_server_error?)
   end
 
 end
