4ecd92fb8a03f464377755a2524be1bb4789c2e5
George Guimarães <george@plataformatec.com.br>
2010-01-13 17:00:49 -0200

    using constants instead of creating new objects each time this method is called

--- lib/restfulie/client/base.rb
+++ lib/restfulie/client/base.rb
@@ -20,18 +20,17 @@ module Restfulie
   
   module Client
     module Config
+      BASIC_MAPPING = { :delete => Net::HTTP::Delete, :put => Net::HTTP::Put, :get => Net::HTTP::Get, :post => Net::HTTP::Post}
+      DEFAULTS = { :destroy => Net::HTTP::Delete, :delete => Net::HTTP::Delete, :cancel => Net::HTTP::Delete,
+                   :refresh => Net::HTTP::Get, :reload => Net::HTTP::Get, :show => Net::HTTP::Get, :latest => Net::HTTP::Get, :self => Net::HTTP::Get}
 
       def self.self_retrieval
         [:latest, :refresh, :reload, :self]
       end
   
       def self.requisition_method_for(overriden_option,name)
-        basic_mapping = { :delete => Net::HTTP::Delete, :put => Net::HTTP::Put, :get => Net::HTTP::Get, :post => Net::HTTP::Post}
-        return basic_mapping[overriden_option.to_sym] if overriden_option
-
-        defaults = {:destroy => Net::HTTP::Delete, :delete => Net::HTTP::Delete, :cancel => Net::HTTP::Delete,
-                    :refresh => Net::HTTP::Get, :reload => Net::HTTP::Get, :show => Net::HTTP::Get, :latest => Net::HTTP::Get, :self => Net::HTTP::Get}
-        defaults[name.to_sym] || Net::HTTP::Post
+        return BASIC_MAPPING[overriden_option.to_sym] if overriden_option
+        DEFAULTS[name.to_sym] || Net::HTTP::Post
       end
     
     end
