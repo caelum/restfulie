e1a0605779589fb747e693f744250ca831a69bb6
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-01-14 22:04:24 -0200

    test for links methods

--- spec/server/instance_spec.rb
+++ spec/server/instance_spec.rb
@@ -2,12 +2,12 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 context Restfulie::Server::Instance do
   
+  before do
+    @o = Object.new
+    @o.extend Restfulie::Server::Instance
+  end
+
   context "when checking available transitions" do
-    
-    before do
-      @o = Object.new
-      @o.extend Restfulie::Server::Instance
-    end
     
     it "should return true if the object has a transition named after the required string or symbol" do
       hash = Hashi::CustomHash.new("name" => :execute)
@@ -44,6 +44,28 @@ context Restfulie::Server::Instance do
       @o.should_receive(:following_transitions).and_return([available])
       @o.should_receive(:available_transitions).and_return({:allow => []})
       @o.all_following_transitions.should eql([available])
+    end
+    
+  end
+  
+  context "when locating links" do
+    
+    it "should return no link if there is no controller" do
+      @o.links(nil).should eql([])
+    end
+    
+    it "should return a link for every transition" do
+      t1 = Object.new
+      t2 = Object.new
+      controller = Object.new
+      @o.should_receive(:all_following_transitions).and_return([t1, t2])
+      @o.should_receive(:link_for).with(t1, controller).and_return(["rel1", "uri1"])
+      @o.should_receive(:link_for).with(t2, controller).and_return(["rel2", "uri2"])
+      links = @o.links(controller)
+      links[0][:rel].should eql("rel1")
+      links[0][:uri].should eql("uri1")
+      links[1][:rel].should eql("rel2")
+      links[1][:uri].should eql("uri2")
     end
     
   end
