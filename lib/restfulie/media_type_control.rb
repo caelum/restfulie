241d6041b579a088cd30fd061a8f51dd28f3f988
Caue Guerra <caueguerra@gmail.com>
2010-01-20 16:51:23 -0200

    adding license to all rb files

--- lib/restfulie/media_type_control.rb
+++ lib/restfulie/media_type_control.rb
@@ -1,4 +1,20 @@
-# extends Rails mime type
+#
+#  Copyright (c) 2009 Caelum - www.caelum.com.br/opensource
+#  All rights reserved.
+# 
+#  Licensed under the Apache License, Version 2.0 (the "License"); 
+#  you may not use this file except in compliance with the License. 
+#  You may obtain a copy of the License at 
+#  
+#   http://www.apache.org/licenses/LICENSE-2.0 
+#  
+#  Unless required by applicable law or agreed to in writing, software 
+#  distributed under the License is distributed on an "AS IS" BASIS, 
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+#  See the License for the specific language governing permissions and 
+#  limitations under the License. 
+## extends Rails mime type
+
 module Mime
   class << self
     # alias_method :old_const_set, :const_set
@@ -44,7 +60,6 @@ module Restfulie
   
 
   def register(string, symbol, mime_type_synonyms = [], extension_synonyms = [], skip_lookup = false)
-
     SET << Mime.const_get(symbol.to_s.upcase)
 
     ([string] + mime_type_synonyms).each { |string| LOOKUP[string] = SET.last } unless skip_lookup
