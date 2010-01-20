f7790f188655ac4a8e5caa499e9dc6186f7292ec
Caue Guerra <caueguerra@gmail.com>
2010-01-20 17:47:28 -0200

    fixing tests

--- lib/vendor/jeokkarak/jeokkarak.rb
+++ lib/vendor/jeokkarak/jeokkarak.rb
@@ -13,7 +13,8 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
 #  See the License for the specific language governing permissions and 
 #  limitations under the License. 
-#require 'vendor/jeokkarak/hashi'
+
+require 'vendor/jeokkarak/hashi'
  
 module Jeokkarak
   module Base
