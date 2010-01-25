99a140f2777a5bc559202a7b850388c4abd570cd
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-01-25 17:07:33 -0200

    fake cache implemented

--- lib/restfulie/client/cache.rb
+++ lib/restfulie/client/cache.rb
@@ -3,3 +3,14 @@ class Restfulie::BasicCache
   
   
 end
+
+# Fake cache that does not cache anything
+class Restfulie::FakeCache
+  
+  def put(url, req)
+  end
+  
+  def get(url, req)
+  end
+  
+end
