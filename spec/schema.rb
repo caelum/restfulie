94bbbee33ab9c24636878e7fb0167626bfae67c2
George Guimarães <george@plataformatec.com.br>
2010-02-03 20:29:28 -0200

    merging several migrations into one

--- spec/schema.rb
+++ spec/schema.rb
@@ -15,28 +15,19 @@
 #  limitations under the License. 
 #
 
-ActiveRecord::Schema.define :version => 0 do
-  create_table "restfulie_models", :force => true do |t|
-    t.column :status, :string
+ActiveRecord::Schema.define(:version => 1) do
+  [:restfulie_models, :client_restfulie_models].each do |table|
+    create_table table, :force => true do |t|
+      t.column :status, :string
+    end
   end
-  create_table "client_restfulie_models", :force => true do |t|
-    t.column :status, :string
+  
+  [:orders, :client_orders, :people, :teams].each do |table|
+    create_table table, :force => true do 
+    end
   end
-end
-ActiveRecord::Schema.define :version => 1 do
-  create_table "orders", :force => true do |t|
-  end
-  create_table "client_orders", :force => true do |t|
-  end
-end
-ActiveRecord::Schema.define :version => 2 do
-  create_table "people", :force => true do |t|
-  end
-end
-ActiveRecord::Schema.define :version => 3 do
+  
   create_table "players", :force => true do |t|
     t.column :name, :string
-  end
-  create_table "teams", :force => true do |t|
   end
 end
