74c9b0eb2c1963b99f0c5a8f786f1e5b492fb8a1
Everton Ribeiro <nuxlli@gmail.com>
2010-02-24 17:09:34 -0300

    Add load_serializer in Serializer class.

--- lib/restfulie/common/serializer.rb
+++ lib/restfulie/common/serializer.rb
@@ -1,20 +1,3 @@
-#
-#  Copyright (c) 2009 Caelum - www.caelum.com.br/opensource
-#  All rights reserved.
-# 
-#  Licensed under the Apache License, Version 2.0 (the "License"); 
-#  you may not use this file except in compliance with the License. 
-#  You may obtain a copy of the License at 
-#  
-#   http://www.apache.org/licenses/LICENSE-2.0 
-#  
-#  Unless required by applicable law or agreed to in writing, software 
-#  distributed under the License is distributed on an "AS IS" BASIS, 
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-#  See the License for the specific language governing permissions and 
-#  limitations under the License.
-#
-
 module Restfulie::Serializer
   @@serializers_path = [File.join(File.dirname(__FILE__), 'serializers')]
   mattr_accessor :serializers_path
@@ -46,6 +29,15 @@ module Restfulie::Serializer
       load_serializers
     end
     
+    def load_serializer(serializer_name)
+      serializer_name = serializer_name.to_s.capitalize
+      begin
+        "#{self.ancestors.first}::#{serializer_name}".constantize
+      rescue NameError
+        raise Restfulie::Error::UndefinedSerializerError.new("Serializer #{serializer_name} not fould.")
+      end
+    end
+    
   private
 
     def parse_name(method)
@@ -67,11 +59,7 @@ module Restfulie::Serializer
     end
 
     def initialize_serializer(serializer_name, *args)
-      begin
-        "#{self.ancestors.first}::#{serializer_name.capitalize}".constantize.new(*args)
-      rescue NameError
-        raise Restfulie::Error::UndefinedSerializerError.new("Serializer #{serializer_name.capitalize} not fould.")
-      end
+      load_serializer(serializer_name).new(*args).to_s
     end
 
   end
--- spec/common/serializer_spec.rb
+++ spec/common/serializer_spec.rb
@@ -12,38 +12,42 @@ context ::Restfulie::Serializer do
   it "should not replace native to methods" do
     ::Restfulie::Serializer.should_not respond_to(:to_s)
   end
-
-  context "serializers_path" do
-
-    it "should have a attribute and attribute should array" do
+  
+  context "adicional serializers" do
+    it "should have a attribute serializers_path and attribute should array" do
       ::Restfulie::Serializer.should respond_to(:serializers_path)
       ::Restfulie::Serializer.serializers_path.should be_kind_of(Array)
     end
 
     it "should verify the existence of serializers in the paths" do
-      ::Restfulie::Serializer.should_not respond_to(:to_bar)
       ::Restfulie::Serializer.serializers_path << File.expand_path(File.dirname(__FILE__) + '/../lib/serializers')
       ::Restfulie::Serializer.should respond_to(:to_bar)
     end
+  end # context "adicional serializers"
+  
+  context "load serializers" do
+    before(:all) do
+      ::Restfulie::Serializer.send(:remove_const, :Foo) if ::Restfulie::Serializer.const_defined?(:Foo)
+      ::Restfulie::Serializer.serializers_path << File.expand_path(File.dirname(__FILE__) + '/../lib/serializers')
+    end
+    
+    it "should autoload the serializers" do
+      ::Restfulie::Serializer.to_foo([])
+      ::Restfulie::Serializer.should be_const_defined(:Foo)
+    end
 
-  end # context "serializers_path"
-
-  it "should autoload the serializers" do
-    ::Restfulie::Serializer.send(:remove_const, :Foo) if ::Restfulie::Serializer.const_defined?(:Foo)
-    ::Restfulie::Serializer.serializers_path << File.expand_path(File.dirname(__FILE__) + '/../lib/serializers')
-    ::Restfulie::Serializer.to_foo([])
-    ::Restfulie::Serializer.should be_const_defined(:Foo)
-  end
+    it "should allow manual loading of serializer" do
+      ::Restfulie::Serializer.load_serializer(:foo)
+      ::Restfulie::Serializer.should be_const_defined(:Foo)
+    end
+    
+  end # context "load serializers"
   
   it "should raiser error for a serializer not found" do
     msg = "Serializer Bar not fould."
     lambda {
-      ::Restfulie::Serializer.to_bar([])
+      ::Restfulie::Serializer.load_serializer(:bar)
     }.should raise_error(::Restfulie::Error::UndefinedSerializerError, msg)
-  end
-  
-  it "should return Atom object to call to_atom" do
-    ::Restfulie::Serializer.to_atom([]).should be_kind_of(::Restfulie::Serializer::Atom)
   end
 
 end # context ::Restfulie::Serializer
