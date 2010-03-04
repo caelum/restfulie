07e03eeead2275e4538e90764e75e5d7a27ae811
George Guimarães <george@plataformatec.com.br>
2010-03-04 14:54:31 -0300

    Moving template registration. Don't use layout for tokamak files.

--- lib/restfulie/server/template_handlers/tokamak.rb
+++ lib/restfulie/server/template_handlers/tokamak.rb
@@ -4,14 +4,8 @@ module Restfulie::Server::TemplateHandlers
     include ActionView::TemplateHandlers::Compilable
       
     def compile(template)
-      "_set_controller_content_type(Mime::ATOM); " +
+      # "_set_controller_content_type(Mime::ATOM); " +
       template.source
     end
   end
 end
-
-if defined? ActionView::Template and ActionView::Template.respond_to? :register_template_handler
-  ActionView::Template
-else
-  ActionView::Base
-end.register_template_handler(:tokamak, ::Restfulie::Server::TemplateHandlers::Tokamak)
