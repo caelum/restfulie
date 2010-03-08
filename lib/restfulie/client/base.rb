07b9393f00fe8b9616b3f5a665f9981ef029fcfd
Paulo Ahagon & Luis Cipriani <lfcipriani@gmail.com>
2010-03-08 14:38:02 -0300

    initial documentation

--- lib/restfulie/client/base.rb
+++ lib/restfulie/client/base.rb
@@ -1,5 +1,33 @@
+# == Retrieving resources
+# 
+# There is 2 ways to initially access a resource:
+# * Using a class that includes Base module, which is more configurable:
+# 
+#     class Post                                                        
+#       include Restfulie::Client::Base                                 
+#                                                                   
+#       uses_restfulie do |config|                                      
+#         config.entry_point     = 'http://resource.entrypoint.com/post'
+#         config.default_headers = {                                    
+#           :get  => { 'Accept'       => 'application/atom+xml' },      
+#           :post => { 'Content-Type' => 'application/atom+xml' }       
+#         }                                                             
+#       end                                                             
+#     end
+#     post = Post.get
+#     post.title #=> "Hello world!"
+# 
+# * Directly accessing an entry point, without using any class: 
+# 
+#     post = Restfulie::Client::EntryPoint.at("http://resource.entrypoint.com/post").get
+#     post.title #=> "Hello world!"
+# 
+# == Configuration
+# 
+# Check configuration options at Configuration class documentation.
 module Restfulie::Client
 
+  # 
   module EntryPoint
     include HTTP::Marshal::RequestBuilder
     extend self
