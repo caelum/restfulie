7f2bdb974cee322ceb1ed241e03af71334f14819
Everton Ribeiro <nuxlli@gmail.com>
2010-03-08 19:20:17 -0300

    Implementing basic helpers for create Resource Rules and Collection Rules.

--- lib/restfulie/common/builder/helpers.rb
+++ lib/restfulie/common/builder/helpers.rb
@@ -1,2 +1,17 @@
 module Restfulie::Builder::Helpers
+
+  def resource(&block)
+    create_rule(Restfulie::Builder::ResourceRules, &block)
+  end
+
+  def collection(&block)
+    create_rule(Restfulie::Builder::CollectionRules, &block)
+  end
+
+private
+
+  def create_rule(rule, *args, &block)
+    rules = block_given? ? [rule.new(&block)] : []
+    Restfulie::Builder::Base.new(rules)
+  end
 end
--- spec/common/builder/helpers_spec.rb
+++ spec/common/builder/helpers_spec.rb
@@ -1,4 +1,43 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../../lib/models')
 
 context Restfulie::Builder::Helpers do
+  include Restfulie::Builder::Helpers
+
+  context "entry" do
+    it "should create a builder for a simple call" do
+      builder = resource()
+      builder.should be_kind_of(Restfulie::Builder::Base)
+    end
+    
+    it "should create a entry rule and set in builder rules" do
+      block   = lambda {}
+      builder = resource(&block)
+      builder.rules.should_not be_blank()
+      builder.rules.first.block.should eql(block)
+    end
+
+    # context "builder a representation" do
+    #   it "should create a simple represenatation" do
+    #     album   = Album.first
+    #     builder = entry(album)
+    #     builder.to_atom().should eql()
+    #   end
+    # end
+    
+  end # context "entry"
+  
+  context "collection" do
+    it "should create a builder for a simple call" do
+      builder = collection()
+      builder.should be_kind_of(Restfulie::Builder::Base)
+    end
+    
+    it "should create a entry rule and set in builder rules" do
+      block   = lambda {}
+      builder = collection(&block)
+      builder.rules.should_not be_blank()
+      builder.rules.first.block.should eql(block)
+    end
+  end # context "entry"
 end
