08090c130155cc97c3774d94d6a3b865f02fe291
Everton Ribeiro <nuxlli@gmail.com>
2010-03-08 19:15:12 -0300

    Implemented Link and Links to Rules.

---
+++ lib/restfulie/common/builder/rules/links.rb
@@ -0,0 +1,9 @@
+class Restfulie::Builder::Rules::Links < Array
+  alias_method :old_delete, :delete
+
+  def delete(item)
+    item = item.to_s if item.kind_of?(Symbol) 
+    deleted = old_delete(item)
+    deleted.nil? ? old_delete(find { |i| i.rel == item }) : deleted
+  end
+end
