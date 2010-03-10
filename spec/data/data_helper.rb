47fbe861037ed134745b8cb9be4fdec23f272696
Everton Ribeiro <nuxlli@gmail.com>
2010-03-10 13:57:52 -0300

    Simplify response_data in data_helper.

--- spec/data/data_helper.rb
+++ spec/data/data_helper.rb
@@ -3,11 +3,7 @@ module DataHelper
   def response_data(format, filename)
     filename = File.join(File.dirname(__FILE__), format.to_s, filename)
     raise "File not found: #{filename}. Check atoms spec folder for available samples." if !File.exist?(filename)
-    file_dump = ""
-    File.open(filename, "r") do |file|
-      file.each_line {|line| file_dump << line }
-    end
-    return file_dump
+    IO.read(filename)
   end
   
 end
