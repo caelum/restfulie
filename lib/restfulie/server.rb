dcf49ea99ebb19ac5655f9ce04d5ab04384512be
Everton Ribeiro e George Guimarães <nuxlli@gmail.com>
2010-03-16 18:38:12 -0300

    Remove active_record extend and move base specs

--- lib/restfulie/server.rb
+++ lib/restfulie/server.rb
@@ -7,7 +7,6 @@ end
   configuration
   action_controller
   action_view
-  active_record
   core_ext
   restfulie_controller
 ).each do |file|
--- lib/restfulie/server/active_record/serializers.rb
+++
@@ -1,5 +0,0 @@
-module Restfulie::Server::ActiveRecord::Serializers #:nodoc:
-end
-
-require 'restfulie/server/active_record/serializers/atom'
-
