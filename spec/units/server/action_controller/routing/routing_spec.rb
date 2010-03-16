d059e765badf5264c91ea43548d214af899ee062
Everton Ribeiro e George Guimarães <nuxlli@gmail.com>
2010-03-16 18:40:42 -0300

    Fixing require of spec_helper in action_controller

--- spec/units/server/action_controller/routing/routing_spec.rb
+++ spec/units/server/action_controller/routing/routing_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/../../../../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../../../spec_helper')
 
 module Restfulie::Server::ActionController::Test
   class AlbumsController < Restfulie::Server::ActionController::Base
