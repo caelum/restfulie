b6a734f103d1c4fafb61a6a5a5283aa7340d9026
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-04-06 11:59:22 -0300

    basic xml implementation

---
+++ lib/restfulie/common/representation/xml.rb
@@ -0,0 +1,25 @@
+module Restfulie::Common::Representation
+  # Implements the interface for marshal Xml media type requests (application/xml)
+  class XmlD
+
+    cattr_reader :media_type_name
+    @@media_type_name = 'application/xml'
+
+    cattr_reader :headers
+    @@headers = { 
+      :get  => { 'Accept'       => media_type_name },
+      :post => { 'Content-Type' => media_type_name }
+    }
+
+    def unmarshal(string)
+      raise "should never be invoked, xml to ruby objects should be handled by rails itself"
+    end
+
+    def marshal(string)
+      string
+    end
+
+  end
+
+end
+
