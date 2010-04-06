bece1495fb89bc8447328378ee2a1b08586d4b06
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-04-06 20:56:36 -0300

    supporting looking up element in extensions

--- lib/restfulie/client/http/atom_ext.rb
+++ lib/restfulie/client/http/atom_ext.rb
@@ -1,12 +1,29 @@
 module Restfulie::Client::HTTP#:nodoc:
 
-  # Offer easily access to Atom link relationships, such as <tt>post.next</tt> for 
+  # Offer easy access to Atom link relationships, such as <tt>post.next</tt> for 
   # <tt>&lt;link rel="next" href="http://resource.entrypoint.com/post/12" type="application/atom+xml" /&gt;</tt> relationships.
   module AtomLinkShortcut
     def method_missing(method_sym,*args)#:nodoc:
       selected_links = links.select{ |l| l.rel == method_sym.to_s }
       super if selected_links.empty?
       selected_links.size == 1 ? selected_links.first : selected_links
+    end
+  end
+  
+  # Offers a way to access Atom entries element's in namespaced extensions.
+  module AtomElementShortcut
+    def method_missing(method_sym,*args)
+      return super(method_sym, *args) unless simple_extensions
+      
+      start = -(method_sym.to_s.length + 1)
+      found = simple_extensions.select do |k, v|
+        method_sym.to_s == k[start..-2]
+      end
+      return super(method_sym, *args) if found.empty?
+      result = found.collect do |pair|
+        pair.last.length==1 ? pair.last.first : pair.last
+      end
+      result.length==1 ? result.first : result
     end
   end
 
@@ -21,7 +38,10 @@ module Restfulie::Client::HTTP#:nodoc:
 
   # inject new behavior in rAtom instances to enable easily access to link relationships.
   ::Atom::Feed.instance_eval { include AtomLinkShortcut }
-  ::Atom::Entry.instance_eval { include AtomLinkShortcut }
+  ::Atom::Entry.instance_eval {
+    include AtomLinkShortcut
+    include AtomElementShortcut
+  }
   ::Atom::Link.instance_eval { include LinkRequestBuilder }
   
 end
