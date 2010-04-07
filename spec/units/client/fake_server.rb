b6ad5d06843ed8f3e2fa9a72d8cdd9febdc9f45f
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-04-07 12:19:10 -0300

    testing different media types on request and response

--- spec/units/client/fake_server.rb
+++ spec/units/client/fake_server.rb
@@ -41,9 +41,21 @@ post '/custom/songs' do
   r.finish
 end
 
+post '/with_content' do
+  r = Rack::Response.new('<?xml version="1.0" encoding="UTF-8"?>
+  <feed xmlns="http://www.w3.org/2005/Atom">
+    <id>http://localhost:3000/</id>
+    <title>NilClasses feed</title>
+    <updated>2010-04-06T22:06:43-03:00</updated>
+    <link href="http://localhost:3000/search_items" rel="search" type="application/atom+xml"/>
+  </feed>
+  ', 200)
+  r.header["Content-type"] = "application/atom+xml"
+  r.finish
+end
+
 get '/:file_name' do
   respond_to do |wants|
     wants.atom { response_data( 'atoms', params[:file_name] ) } 
   end
 end
-
