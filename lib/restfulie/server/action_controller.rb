b656bbcdd164c6a9d46deff52a6cdfb118d842e9
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-04-08 18:29:53 -0300

    fixing actioncontroller definition order

--- lib/restfulie/server/action_controller.rb
+++ lib/restfulie/server/action_controller.rb
@@ -1,15 +1,14 @@
-if defined?(::ActionController) || defined?(::ApplicationController)
+module Restfulie::Server::ActionController#:nodoc:
+  if defined?(::ActionController) || defined?(::ApplicationController)
 
-  module Restfulie::Server::ActionController#:nodoc:
+    %w(
+      params_parser
+      restful_responder
+      base
+      routing
+    ).each do |file|
+      autoload file.camelize.to_sym, "restfulie/server/action_controller/#{file}"
+    end
+
   end
-
-  %w(
-    params_parser
-    restful_responder
-    base
-    routing
-  ).each do |file|
-    autoload file.camelize.to_sym, "restfulie/server/action_controller/#{file}"
-  end
-
 end
