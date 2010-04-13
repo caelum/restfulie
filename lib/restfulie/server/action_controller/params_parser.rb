865f5bbd87079acc634d20df4e79b086d1ad08ee
Luis Cipriani <lfcipriani@gmail.com>
2010-04-13 17:51:22 -0300

    removing temporarily params parser monkey patch, check ticket #38 for more details

--- lib/restfulie/server/action_controller/params_parser.rb
+++ lib/restfulie/server/action_controller/params_parser.rb
@@ -42,21 +42,21 @@ end
 # extensions are handled by the Rack stack
 #
 # TODO Change this when porting this code to Rails 3
-::ActionController::ParamsParser.class_eval do
-  def call(env)
-    begin
-      if params = parse_formatted_parameters(env)
-        env["action_controller.request.request_parameters"] = params
-      else
-        if env["CONTENT_LENGTH"] && (env["CONTENT_LENGTH"] != "0")
-          return [415, {'Content-Type' => 'text/html'}, "<html><body><h1>415 Unsupported Media Type</h1></body></html>"]
-        end
-      end
-    rescue
-      return [400, {'Content-Type' => 'text/html'}, "<html><body><h1>400 Bad Request</h1></body></html>"]
-    end
+#::ActionController::ParamsParser.class_eval do
+  #def call(env)
+    #begin
+      #if params = parse_formatted_parameters(env)
+        #env["action_controller.request.request_parameters"] = params
+      #else
+        #if env["CONTENT_LENGTH"] && (env["CONTENT_LENGTH"] != "0")
+          #return [415, {'Content-Type' => 'text/html'}, "<html><body><h1>415 Unsupported Media Type</h1></body></html>"]
+        #end
+      #end
+    #rescue
+      #return [400, {'Content-Type' => 'text/html'}, "<html><body><h1>400 Bad Request</h1></body></html>"]
+    #end
 
-    @app.call(env)
-  end
-end
+    #@app.call(env)
+  #end
+#end
 
