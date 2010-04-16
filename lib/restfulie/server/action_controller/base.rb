5d1f9a9113d01788c63081216cb31db9da4d3a70
Luis Cipriani <lfcipriani@gmail.com>
2010-04-16 13:34:45 -0300

    avoiding unexpected nil options value (Lucas Fais, Manzan)

--- lib/restfulie/server/action_controller/base.rb
+++ lib/restfulie/server/action_controller/base.rb
@@ -31,7 +31,7 @@ module Restfulie
         # If your controller inherits from Restfulie::Server::Controller::Base,
         # it will have an :atom option, very similar to render :xml
         def render(options = {}, extra_options = {}, &block)
-          if atom = options[:atom]
+          if options && atom = options[:atom]
             response.content_type ||= Mime::ATOM
             render_for_text(atom.respond_to?(:to_atom) ? atom.to_atom.to_xml : atom.to_xml, options[:status])
           else
