13088c46a15ba853f35058dbf220848071e08270
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-04-19 07:10:17 -0300

    removing examples and old guides

--- examples/guide/java/server/src/br/com/caelum/vraptor/restbucks/restfulie/StateMachineController.java
+++
@@ -1,8 +0,0 @@
-package br.com.caelum.vraptor.restbucks.restfulie;
-
-public interface StateMachineController<T> {
-
-	public String getBaseUri();
-	public T retrieve(String id);
-
-}
--- examples/guide/ruby/server/test/unit/helpers/hotels_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class HotelsHelperTest < ActionView::TestCase
-end
