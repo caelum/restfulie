1247d10b16dcd142a592955fd78c9432471c041f
Luis Cipriani & Paulo Ahagon <paulo.ahagon@gmail.com>
2010-04-26 14:29:22 -0300

    Bugfix - load_entry and load_feed when converter trying parse feed or entry string

--- lib/restfulie/common/converter/atom.rb
+++ lib/restfulie/common/converter/atom.rb
@@ -10,24 +10,14 @@ module Restfulie::Common::Converter
     mattr_reader :recipes
     @@recipes = {
       :default_feed => lambda do |obj,feed|
-        if obj.is_a?(::String)
-          feed = ::Atom::Feed.load_feed(obj)
-        else
-          REQUIRED_ATTRIBUTES[:feed].each do |attr_sym|
-            feed.send("#{attr_sym}=".to_sym,obj.send(attr_sym)) if obj.respond_to?(attr_sym)
-          end
+        REQUIRED_ATTRIBUTES[:feed].each do |attr_sym|
+          feed.send("#{attr_sym}=".to_sym,obj.send(attr_sym)) if obj.respond_to?(attr_sym)
         end
-        feed
       end,
       :default_entry => lambda do |obj,entry|
-        if obj.is_a?(::String)
-          entry = ::Atom::Entry.load_entry(obj)
-        else
-          REQUIRED_ATTRIBUTES[:entry].each do |attr_sym|
-            entry.send("#{attr_sym}=".to_sym,obj.send(attr_sym)) if obj.respond_to?(attr_sym)
-          end
+        REQUIRED_ATTRIBUTES[:entry].each do |attr_sym|
+          entry.send("#{attr_sym}=".to_sym,obj.send(attr_sym)) if obj.respond_to?(attr_sym)
         end
-        entry
       end
     }
 
@@ -43,16 +33,36 @@ module Restfulie::Common::Converter
 
     def to_atom(recipe_name=nil,atom_type=:entry)
       raise "Undefined atom type #{atom_type}" unless [:entry,:feed].include?(atom_type)
-      recipe = @@recipes[recipe_name] || @@recipes["default_#{atom_type}".to_sym]
 
       #TODO Code smell.
-      begin
-        atom = "::Atom::#{atom_type.to_s.camelize}".constantize.new
-        recipe.call(self,atom)
-      rescue 
-        atom_type = atom_type == :entry ? :feed : :entry
-        atom = "::Atom::#{atom_type.to_s.camelize}".constantize.new
-        recipe.call(self,atom)
+      if is_a?(::String)
+
+        begin 
+          if atom_type == :entry
+            atom_type = :feed
+            atom = ::Atom::Feed.load_feed(self)
+          else
+            atom_type = :entry
+            atom = ::Atom::Entry.load_entry(self) 
+          end
+        rescue
+          atom_type = atom_type == :entry ? :feed : :entry
+          atom = to_atom(recipe_name,atom_type)
+        end
+
+      else
+
+        recipe = @@recipes[recipe_name] || @@recipes["default_#{atom_type}".to_sym]
+        begin
+          atom = "::Atom::#{atom_type.to_s.camelize}".constantize.new
+          recipe.call(self,atom)
+        rescue 
+          atom_type = atom_type == :entry ? :feed : :entry
+          atom = "::Atom::#{atom_type.to_s.camelize}".constantize.new
+          recipe = @@recipes[recipe_name] || @@recipes["default_#{atom_type}".to_sym]
+          recipe.call(self,atom)
+        end
+
       end
 
       REQUIRED_ATTRIBUTES[atom_type].each do |attr_sym|
