b32097c973a141867a0af04bc21125b49cee04b3
Luis Cipriani <lfcipriani@gmail.com>
2010-05-27 09:46:46 -0300

    Json representation Object, updating constants names and tests

--- lib/restfulie/common/representation/json.rb
+++ lib/restfulie/common/representation/json.rb
@@ -1,25 +1,77 @@
 module Restfulie::Common::Representation
 
-  # Implements the interface for marshal Xml media type requests (application/xml)
-  class Json
-
-    cattr_reader :media_type_name
-    @@media_type_name = 'application/json'
-
-    cattr_reader :headers
-    @@headers = { 
-      :post => { 'Content-Type' => media_type_name }
-    }
-
-    def unmarshal(string)
-      JSON.parse(string)
+  class JSON
+    
+    class << self
+      def create(obj = nil)
+        @json = {}
+        return @json.extend(JSONKeysAsMethods) unless obj
+        
+        if obj.kind_of?(Hash) || obj.kind_of?(Array)
+          @json = obj
+        else 
+          @json = ::JSON.parse(obj)
+        end
+        
+        @json.extend(JSONKeysAsMethods)
+      end
     end
-
-    def marshal(entity, rel)
-      return entity if entity.kind_of? String
-      entity.to_json
-    end
-
+    
   end
+  
+  module JSONKeysAsMethods
 
+    # overriding the already deprecated method :type
+    alias_method :__type__, :type
+    def type
+      method_missing(:type)
+    end
+    
+    def [](key)
+      __normalize__(super(key))
+    end
+
+    def []=(key, value)
+      super(key,value)
+    end
+
+    def to_s
+      super.to_json
+    end
+
+    def method_missing(name, *args)
+      method_name = name.to_s
+      if method_name.last == '='
+        self[method_name.chop] = args[0]
+      else
+        self[method_name]
+      end
+    end
+
+    # if you have a key that is also a method (such as Array#size) 
+    # you can use this to free the method and use the method obj.size
+    # to access the value of key "size".
+    # you still can access the old method with __[method_name]__
+    def __free_method__(sym)
+      self.class.send(:alias_method, "__#{sym.to_s}__".to_sym, sym)
+      self.class.send(:define_method, sym) { method_missing(sym.to_s) }
+      self
+    end
+
+  private
+
+    def __normalize__(value)
+      case value
+      when Hash
+        value.extend(JSONKeysAsMethods)
+      when Array
+        value.map { |v| __normalize__(v) }
+      else
+        value
+      end
+      value
+    end
+    
+  end
+  
 end
