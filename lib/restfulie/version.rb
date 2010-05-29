a0f0b2fbd23d59630fadfc1b66d2d4247416f6e9
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-05-29 14:43:03 -0300

    preparing 0.8.1

--- lib/restfulie/version.rb
+++ lib/restfulie/version.rb
@@ -2,10 +2,11 @@ module Restfulie
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 8
-    TINY  = 0
+    TINY  = 1
 
     STRING = [MAJOR, MINOR, TINY].join('.')
 
+    # Restfulie's version
     def self.to_s
       STRING
     end
