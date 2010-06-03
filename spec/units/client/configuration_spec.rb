c4b608e20ec198da4aec47ac125395ded60fede3
Tomas D'Stefano <tomasdestefi@gmail.com>
2010-06-03 18:07:11 -0300

    One more refactoring to isolate behaviors

--- spec/units/client/configuration_spec.rb
+++ spec/units/client/configuration_spec.rb
@@ -21,24 +21,35 @@ context Restfulie::Client::Configuration do
     configuration.representations.should == {}
   end
   
+  it "should allow to set the test environment" do
+    configuration.environment = :test
+    configuration.environment.should equal :test
+  end
+  
+  it "should set the atom representations" do
+    configuration.representations['application/atom+xml'] = Restfulie::Client::Configuration::Test::AtomRepresentation
+    configuration.representations['application/atom+xml'].should == Restfulie::Client::Configuration::Test::AtomRepresentation
+  end
+  
+  it "should allow a xml representation" do
+    configuration.representations['application/xml'] = Restfulie::Client::Configuration::Test::XmlRepresentation
+    configuration.representations['application/xml'].should == Restfulie::Client::Configuration::Test::XmlRepresentation    
+  end
+  
   it 'should allow change values for many environments' do
     configuration.entry_point = "http://foo.com/bar"
     configuration.entry_point.should == 'http://foo.com/bar'
         
     configuration.representations['application/atom+xml'] = Restfulie::Client::Configuration::Test::AtomRepresentation
-    configuration.representations['application/atom+xml'].should == Restfulie::Client::Configuration::Test::AtomRepresentation
     
     configuration.environment = :test
     
-    configuration.environment.should == :test
     configuration.entry_point.should be_empty
     configuration.representations.should be_empty
     
     configuration.entry_point = "http://bar.com/foo"
-    configuration.entry_point.should == 'http://bar.com/foo'
     
     configuration.representations['application/xml'] = Restfulie::Client::Configuration::Test::XmlRepresentation
-    configuration.representations['application/xml'].should == Restfulie::Client::Configuration::Test::XmlRepresentation
     
     configuration.environment = :development
     configuration.entry_point.should == 'http://foo.com/bar'
