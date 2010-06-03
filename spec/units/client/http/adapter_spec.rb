259ec534d00c53420467f4fcff10e8415d538050
Tomas D'Stefano <tomasdestefi@gmail.com>
2010-06-03 18:14:52 -0300

    One more Refactoring

--- spec/units/client/http/adapter_spec.rb
+++ spec/units/client/http/adapter_spec.rb
@@ -139,30 +139,34 @@ context Restfulie::Client::HTTP do
     end
     ::Restfulie::Client::HTTP::ResponseHandler.register(701,FakeResponse)
 
-    before(:all) do
-      @client = ::Restfulie::Client::HTTP::RequestExecutor.new(::URI.parse("http://localhost:4567"))
-    end
+    let(:client) { ::Restfulie::Client::HTTP::RequestExecutor.new(::URI.parse("http://localhost:4567")) }
 
     it 'should have FakeResponder as Response Handler to 201' do
-      ::Restfulie::Client::HTTP::ResponseHandler.handlers(701).should == FakeResponse
+      ::Restfulie::Client::HTTP::ResponseHandler.handlers(701).should equal FakeResponse
     end
 
     it 'should respond FakeResponse' do
-      @client.get('/test/701').class.should == FakeResponse
+      client.get('/test/701').class.should equal FakeResponse
     end
 
     it 'should respond default Response' do
-      @client.get('/test/299').class.should == ::Restfulie::Client::HTTP::Response
+      client.get('/test/299').class.should equal ::Restfulie::Client::HTTP::Response
     end
 
   end
 
   context "redirection" do
+    
+    let(:resp) { Restfulie.at("http://localhost:4567/test_redirection").follow.get! }
+    
     it "should follow redirection" do
-      resp = Restfulie.at("http://localhost:4567/test_redirection").follow.get!
       resp.response.path.should == "/redirected"
+    end
+    
+    it "should set the body as 'OK'" do
       resp.response.body.should == "OK"
     end
+    
   end
 
   context "error conditions" do
@@ -174,82 +178,82 @@ context Restfulie::Client::HTTP do
 
     it "raise Error::Redirection error when 300..399  code is returned" do
       @client.get("/test/302").code.should == 302
-      lambda { @client.get!("/test/302") }.should raise_error ::Restfulie::Client::HTTP::Error::Redirection
+      lambda { @client.get!("/test/302") }.should raise_exception ::Restfulie::Client::HTTP::Error::Redirection
     end
 
     it "raise Error::BadRequest error when 400 code is returned" do
       @client.get("/test/400").code.should == 400
-      lambda { @client.get!("/test/400") }.should raise_error ::Restfulie::Client::HTTP::Error::BadRequest
+      lambda { @client.get!("/test/400") }.should raise_exception ::Restfulie::Client::HTTP::Error::BadRequest
     end
 
     it "raise Error::Unauthorized error when 401 code is returned" do
       @client.get("/test/401").code.should == 401
-      lambda { @client.get!("/test/401") }.should raise_error ::Restfulie::Client::HTTP::Error::Unauthorized
+      lambda { @client.get!("/test/401") }.should raise_exception ::Restfulie::Client::HTTP::Error::Unauthorized
     end
 
     it "raise Error::Forbidden error when 403 code is returned" do
       @client.get("/test/403").code.should == 403
-      lambda { @client.get!("/test/403") }.should raise_error ::Restfulie::Client::HTTP::Error::Forbidden
+      lambda { @client.get!("/test/403") }.should raise_exception ::Restfulie::Client::HTTP::Error::Forbidden
     end
 
     it "raise Error::NotFound error when 404 code is returned" do
       @client.get("/test/404").code.should == 404
-      lambda { @client.get!("/test/404") }.should raise_error ::Restfulie::Client::HTTP::Error::NotFound
+      lambda { @client.get!("/test/404") }.should raise_exception ::Restfulie::Client::HTTP::Error::NotFound
     end
 
     it "raise Error::MethodNotAllowed error when 405 code is returned" do
       @client.get("/test/405").code.should == 405
-      lambda { @client.get!("/test/405") }.should raise_error ::Restfulie::Client::HTTP::Error::MethodNotAllowed
+      lambda { @client.get!("/test/405") }.should raise_exception ::Restfulie::Client::HTTP::Error::MethodNotAllowed
     end
 
     it "raise Error::ProxyAuthenticationRequired error when 407 code is returned" do
       @client.get("/test/407").code.should == 407
-      lambda { @client.get!("/test/407") }.should raise_error ::Restfulie::Client::HTTP::Error::ProxyAuthenticationRequired
+      lambda { @client.get!("/test/407") }.should raise_exception ::Restfulie::Client::HTTP::Error::ProxyAuthenticationRequired
     end
 
     it "raise Error::Conflict error when 409 code is returned" do
       @client.get("/test/409").code.should == 409
-     lambda { @client.get!("/test/409") }.should raise_error ::Restfulie::Client::HTTP::Error::Conflict
+     lambda { @client.get!("/test/409") }.should raise_exception ::Restfulie::Client::HTTP::Error::Conflict
     end
 
     it "raise Error::Gone error when 410 code is returned" do
       @client.get("/test/410").code.should == 410
-     lambda { @client.get!("/test/410") }.should raise_error ::Restfulie::Client::HTTP::Error::Gone
+     lambda { @client.get!("/test/410") }.should raise_exception ::Restfulie::Client::HTTP::Error::Gone
     end
 
     it "raise Error::PreconditionFailed error when 412 code is returned" do
       @client.get("/test/412").code.should == 412
-      lambda { @client.get!("/test/412") }.should raise_error ::Restfulie::Client::HTTP::Error::PreconditionFailed
+      lambda { @client.get!("/test/412") }.should raise_exception ::Restfulie::Client::HTTP::Error::PreconditionFailed
     end
 
    it "raise Error::ClientError error when 413 code is returned" do
       @client.get("/test/413").code.should == 413
-      lambda { @client.get!("/test/413") }.should raise_error ::Restfulie::Client::HTTP::Error::ClientError
+      lambda { @client.get!("/test/413") }.should raise_exception ::Restfulie::Client::HTTP::Error::ClientError
     end
 
    it "raise Error::NotImplemented error when 501 code is returned" do
       @client.get("/test/501").code.should == 501
-      lambda { @client.get!("/test/501") }.should raise_error ::Restfulie::Client::HTTP::Error::NotImplemented
+      lambda { @client.get!("/test/501") }.should raise_exception ::Restfulie::Client::HTTP::Error::NotImplemented
     end
 
    it "raise Error::ServerError error when 500 code is returned" do
       @client.get("/test/500").code.should == 500
-      lambda { @client.get!("/test/500") }.should raise_error ::Restfulie::Client::HTTP::Error::ServerError
+      lambda { @client.get!("/test/500") }.should raise_exception ::Restfulie::Client::HTTP::Error::ServerError
     end
 
    it "raise Error::ServerNotAvailableError error when 503 code is returned" do
       @client.get(nil,nil).code.should == 503
-      lambda { @client.get!(nil,nil) }.should raise_error ::Restfulie::Client::HTTP::Error::ServerNotAvailableError
+      lambda { @client.get!(nil,nil) }.should raise_exception ::Restfulie::Client::HTTP::Error::ServerNotAvailableError
     end
 
    it "raise Error::ServerError error when 502..599 code is returned" do
      @client.get("/test/502").code.should == 502
-      lambda { @client.get!("/test/502") }.should raise_error ::Restfulie::Client::HTTP::Error::ServerError
+      lambda { @client.get!("/test/502") }.should raise_exception ::Restfulie::Client::HTTP::Error::ServerError
     end
 
    it "raise Error::UnknownError error when 600 or bigger code is returned" do
      @client.get("/test/600").code.should == 600
-      lambda { @client.get!("/test/600") }.should raise_error ::Restfulie::Client::HTTP::Error::UnknownError
+      lambda { @client.get!("/test/600") }.should raise_exception ::Restfulie::Client::HTTP::Error::UnknownError
     end
 
   end
