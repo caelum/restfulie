c9c97c96829da21d09b7417f002e18573bafce0f
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-06-10 20:55:29 -0300

    seeding out world with data

--- full-examples/rest_from_scratch/part_3/db/seeds.rb
+++ full-examples/rest_from_scratch/part_3/db/seeds.rb
@@ -5,3 +5,8 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+Item.create :name => "Caelum Rest Training", :price => "400"
+Item.create :name => "Water", :price => "2"
+Item.create :name => "Rest in veneza", :price => "800"
+Item.create :name => "Calpis", :price => "10"
