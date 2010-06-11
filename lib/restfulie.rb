8cf16dae8aef769a357a1ee509ccebe92d022a36
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-06-11 10:45:15 -0300

    rdoc

--- lib/restfulie.rb
+++ lib/restfulie.rb
@@ -7,7 +7,8 @@ require 'restfulie/server'
 # Shortcut to Restfulie::Client::EntryPoint
 module Restfulie
   extend Restfulie::Client::EntryPoint
-  
+
+  # creates a new entry point for executing requests
   def self.at(uri)
     Object.new.send(:extend, Restfulie::Client::EntryPoint).at(uri)
   end
