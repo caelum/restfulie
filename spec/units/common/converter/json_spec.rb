063036ff1289e932a87b688115380bfcface8f92
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-06-14 11:58:08 -0300

    requiring json

--- spec/units/common/converter/json_spec.rb
+++ spec/units/common/converter/json_spec.rb
@@ -1,5 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 require File.expand_path(File.dirname(__FILE__) + '/../../lib/models')
+require 'json'
 
 module Restfulie::Common::Converter::Test
   class SimpleClass
