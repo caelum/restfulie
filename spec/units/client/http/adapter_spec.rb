e764e279d19eaa034476745dbd047cb3c05f2f78
Caike Souza <carloshrsouza@gmail.com>
2010-06-30 20:45:54 -0400

    little bit easier to read specs

--- spec/units/client/http/adapter_spec.rb
+++ spec/units/client/http/adapter_spec.rb
@@ -10,39 +10,39 @@ context Restfulie::Client::HTTP do
     end
 
     it "should get and respond 200 code" do
-      @client.get("/test")[2].code.to_i.should == 200
+      @client.get("/test").should respond_with_status(200)
     end
 
     it "should put and respond 200 code" do
-      @client.put("/test", "test")[2].code.to_i.should == 200
+      @client.put("/test", "test").should respond_with_status(200)
     end
 
     it "should delete and respond 200 code" do
-      @client.delete("/test")[2].code.to_i.should == 200
+      @client.delete("/test").should respond_with_status(200)
     end
 
     it "should head and respond 200 code" do
-      @client.head("/test")[2].code.to_i.should == 200
+      @client.head("/test").should respond_with_status(200)
     end
 
     it "should get! and respond 200 code" do
-      @client.get!("/test")[2].code.to_i.should == 200
+      @client.get!("/test").should respond_with_status(200)
     end
 
     it "should post! and respond 201 code" do
-      @client.post!("/test", "test")[2].code.to_i.should == 201
+      @client.post!("/test", "test").should respond_with_status(201)
     end
 
     it "should put! and respond 200 code" do
-      @client.put!("/test", "test")[2].code.to_i.should == 200
+      @client.put!("/test", "test").should respond_with_status(200)
     end
 
     it "should delete! and respond 200 code" do
-      @client.delete!("/test")[2].code.to_i.should == 200
+      @client.delete!("/test").should respond_with_status(200)
     end
 
     it "should head! and respond 200 code" do
-      @client.head!("/test")[2].code.to_i.should == 200
+      @client.head!("/test").should respond_with_status(200)
     end
 
   end
@@ -54,73 +54,73 @@ context Restfulie::Client::HTTP do
     context "On GET" do
       
       it "should respond to 200 code" do
-        builder.at('/test/200').get[2].code.to_i.should == 200
+        builder.at('/test/200').get.should respond_with_status(200)
       end
       
       it "should accepts and respond to 200 code on xml" do
-        builder.at('/test/200').accepts('application/xml').get[2].code.to_i.should == 200
+        builder.at('/test/200').accepts('application/xml').get.should respond_with_status(200)
       end
       
       it "should respond to 200 code as xml" do
-        builder.at('/test/200').as('application/xml').get[2].code.to_i.should == 200
+        builder.at('/test/200').as('application/xml').get.should respond_with_status(200)
       end
       
       it "should accept language and respond 200" do
-        builder.at('/test/200').with('Accept-Language' => 'en').get[2].code.to_i.should == 200
+        builder.at('/test/200').with('Accept-Language' => 'en').get.should respond_with_status(200)
       end
       
       it "should respond 200 code as xml and accept atom and language" do
-        builder.at('/test/200').as('application/xml').accepts('application/atom+xml').with('Accept-Language' => 'en').get[2].code.to_i.should == 200
+        builder.at('/test/200').as('application/xml').accepts('application/atom+xml').with('Accept-Language' => 'en').get.should respond_with_status(200)
       end
       
     end
     
     context "On PUT" do
       it "should respond to 200 code" do
-        builder.at("/test").put("test")[2].code.to_i.should == 200
+        builder.at("/test").put("test").should respond_with_status(200)
       end
       
       it "should accepts xml and respond to 200 code" do
-        builder.at('/test').accepts('application/xml').put("test")[2].code.to_i.should == 200
+        builder.at('/test').accepts('application/xml').put("test").should respond_with_status(200)
       end
       
       it "should respond to 200 code as xml" do
-        builder.at('/test').as('application/xml').put("test")[2].code.to_i.should == 200
+        builder.at('/test').as('application/xml').put("test").should respond_with_status(200)
       end
       
       it "should include accept language and respond 200" do
-        builder.at('/test').with('Accept-Language' => 'en').put("test")[2].code.to_i.should == 200
+        builder.at('/test').with('Accept-Language' => 'en').put("test").should respond_with_status(200)
       end
       
       it "should respond 200 code as xml and accepts xml with en language" do
-        builder.at('/test').as('application/xml').accepts('application/atom+xml').with('Accept-Language' => 'en').put("test")[2].code.to_i.should == 200
+        builder.at('/test').as('application/xml').accepts('application/atom+xml').with('Accept-Language' => 'en').put("test").should respond_with_status(200)
       end
       
       it "should respond 200 code as xml and accept atom and language" do
-        builder.at('/test').as('application/xml').accepts('application/atom+xml').with('Accept-Language' => 'en').put!("test")[2].code.to_i.should == 200
+        builder.at('/test').as('application/xml').accepts('application/atom+xml').with('Accept-Language' => 'en').put!("test").should respond_with_status(200)
       end
     end
     
     context "On DELETE" do
       
       it "should respond 200 code" do
-        builder.at("/test").delete[2].code.to_i.should == 200
+        builder.at("/test").delete.should respond_with_status(200)
       end
       
       it "should accepts xml and respond 200 code" do
-        builder.at('/test').accepts('application/xml').delete[2].code.to_i.should == 200
+        builder.at('/test').accepts('application/xml').delete.should respond_with_status(200)
       end
       
       it "as xml should respond 200 code" do
-        builder.at('/test').as('application/xml').delete[2].code.to_i.should == 200
+        builder.at('/test').as('application/xml').delete.should respond_with_status(200)
       end
       
       it "should respond 200 code with accept language en" do
-        builder.at('/test').with('Accept-Language' => 'en').delete[2].code.to_i.should == 200
+        builder.at('/test').with('Accept-Language' => 'en').delete.should respond_with_status(200)
       end
       
       it "should respond 200 code as xml accepts atom+xml with accept language en" do
-        builder.at('/test').as('application/xml').accepts('application/atom+xml').with('Accept-Language' => 'en').delete[2].code.to_i.should == 200
+        builder.at('/test').as('application/xml').accepts('application/atom+xml').with('Accept-Language' => 'en').delete.should respond_with_status(200)
       end
       
     end
@@ -128,27 +128,27 @@ context Restfulie::Client::HTTP do
     context "On HEAD" do
       
       it "should respond 200 code" do
-        builder.at("/test").head[2].code.to_i.should == 200 
+        builder.at("/test").head.should respond_with_status(200) 
       end
       
       it "should respond 200 code and accepts xml" do
-        builder.at('/test').accepts('application/xml').head[2].code.to_i.should == 200 
+        builder.at('/test').accepts('application/xml').head.should respond_with_status(200) 
       end
       
       it "should respond 200 code as xml" do
-        builder.at('/test').as('application/xml').head[2].code.to_i.should == 200
+        builder.at('/test').as('application/xml').head.should respond_with_status(200)
       end
       
       it "should respond 200 code with accept language en" do
-        builder.at('/test').with('Accept-Language' => 'en').head[2].code.to_i.should == 200
+        builder.at('/test').with('Accept-Language' => 'en').head.should respond_with_status(200)
       end
       
       it "should respond 200 code as xml and accepts atom+xml with accepts language en" do
-        builder.at('/test').as('application/xml').accepts('application/atom+xml').with('Accept-Language' => 'en').head[2].code.to_i.should == 200
+        builder.at('/test').as('application/xml').accepts('application/atom+xml').with('Accept-Language' => 'en').head.should respond_with_status(200)
       end
       
       it "should respond 200 code as xml and accepts atom+xml with accepts language en in a destructive method" do
-        builder.at('/test').as('application/xml').accepts('application/atom+xml').with('Accept-Language' => 'en').head![2].code.to_i.should == 200
+        builder.at('/test').as('application/xml').accepts('application/atom+xml').with('Accept-Language' => 'en').head!.should respond_with_status(200)
       end
       
     end
@@ -163,31 +163,31 @@ context Restfulie::Client::HTTP do
     end
 
     it "should remember last requests" do
-      @builder.at('/test').accepts('application/atom+xml').with('Accept-Language' => 'en').get[2].code.to_i.should == 200
-      @builder.at('/test').accepts('text/html').with('Accept-Language' => 'pt-BR').head[2].code.to_i.should == 200
-      @builder.at('/test').as('application/xml').with('Accept-Language' => 'en').post('test')[2].code.to_i.should == 201
-      @builder.at('/test/500').accepts('application/xml').with('Accept-Language' => 'en').get[2].code.should == 500
+      @builder.at('/test').accepts('application/atom+xml').with('Accept-Language' => 'en').get.should respond_with_status(200)
+      @builder.at('/test').accepts('text/html').with('Accept-Language' => 'pt-BR').head.should respond_with_status(200)
+      @builder.at('/test').as('application/xml').with('Accept-Language' => 'en').post('test').should respond_with_status(201)
+      @builder.at('/test/500').accepts('application/xml').with('Accept-Language' => 'en').get.should respond_with_status(500)
 
-      @builder.history(0).request[2].code.should == 200
+      @builder.history(0).request.should respond_with_status(200)
       @builder.path.should == '/test'
       @builder.host.to_s.should == @host
       @builder.headers['Accept'].should == 'application/atom+xml'
       @builder.headers['Accept-Language'].should == 'en'
 
-      @builder.history(1).request[2].code.should == 200
+      @builder.history(1).request.should respond_with_status(200)
       @builder.path.should == '/test'
       @builder.host.to_s.should == @host
       @builder.headers['Accept'].should == 'text/html'
       @builder.headers['Accept-Language'].should == 'pt-BR'
 
-      @builder.history(2).request[2].code.should == 201
+      @builder.history(2).request.should respond_with_status(201)
       @builder.path.should == '/test'
       @builder.host.to_s.should == @host
       @builder.headers['Accept'].should == 'application/xml'
       @builder.headers['Accept-Language'].should == 'en'
       @builder.headers['Content-Type'].should == 'application/xml'
 
-      @builder.history(3).request[2].code.should == 500
+      @builder.history(3).request.should respond_with_status(500)
       @builder.path.should == '/test/500'
       @builder.host.to_s.should == @host
       @builder.headers['Accept'].should == 'application/xml'
@@ -242,82 +242,82 @@ context Restfulie::Client::HTTP do
     end
 
     it "raise Error::Redirection error when 300..399  code is returned" do
-      @client.get("/test/302")[2].code.should == 302
+      @client.get("/test/302").should respond_with_status(302)
       lambda { @client.get!("/test/302") }.should raise_exception ::Restfulie::Client::HTTP::Error::Redirection
     end
 
     it "raise Error::BadRequest error when 400 code is returned" do
-      @client.get("/test/400")[2].code.should == 400
+      @client.get("/test/400").should respond_with_status(400)
       lambda { @client.get!("/test/400") }.should raise_exception ::Restfulie::Client::HTTP::Error::BadRequest
     end
 
     it "raise Error::Unauthorized error when 401 code is returned" do
-      @client.get("/test/401")[2].code.should == 401
+      @client.get("/test/401").should respond_with_status(401)
       lambda { @client.get!("/test/401") }.should raise_exception ::Restfulie::Client::HTTP::Error::Unauthorized
     end
 
     it "raise Error::Forbidden error when 403 code is returned" do
-      @client.get("/test/403")[2].code.should == 403
+      @client.get("/test/403").should respond_with_status(403)
       lambda { @client.get!("/test/403") }.should raise_exception ::Restfulie::Client::HTTP::Error::Forbidden
     end
 
     it "raise Error::NotFound error when 404 code is returned" do
-      @client.get("/test/404")[2].code.should == 404
+      @client.get("/test/404").should respond_with_status(404)
       lambda { @client.get!("/test/404") }.should raise_exception ::Restfulie::Client::HTTP::Error::NotFound
     end
 
     it "raise Error::MethodNotAllowed error when 405 code is returned" do
-      @client.get("/test/405")[2].code.should == 405
+      @client.get("/test/405").should respond_with_status(405)
       lambda { @client.get!("/test/405") }.should raise_exception ::Restfulie::Client::HTTP::Error::MethodNotAllowed
     end
 
     it "raise Error::ProxyAuthenticationRequired error when 407 code is returned" do
-      @client.get("/test/407")[2].code.should == 407
+      @client.get("/test/407").should respond_with_status(407)
       lambda { @client.get!("/test/407") }.should raise_exception ::Restfulie::Client::HTTP::Error::ProxyAuthenticationRequired
     end
 
     it "raise Error::Conflict error when 409 code is returned" do
-      @client.get("/test/409")[2].code.should == 409
+      @client.get("/test/409").should respond_with_status(409)
      lambda { @client.get!("/test/409") }.should raise_exception ::Restfulie::Client::HTTP::Error::Conflict
     end
 
     it "raise Error::Gone error when 410 code is returned" do
-      @client.get("/test/410")[2].code.should == 410
+      @client.get("/test/410").should respond_with_status(410)
      lambda { @client.get!("/test/410") }.should raise_exception ::Restfulie::Client::HTTP::Error::Gone
     end
 
     it "raise Error::PreconditionFailed error when 412 code is returned" do
-      @client.get("/test/412")[2].code.should == 412
+      @client.get("/test/412").should respond_with_status(412)
       lambda { @client.get!("/test/412") }.should raise_exception ::Restfulie::Client::HTTP::Error::PreconditionFailed
     end
 
    it "raise Error::ClientError error when 413 code is returned" do
-      @client.get("/test/413")[2].code.should == 413
+      @client.get("/test/413").should respond_with_status(413)
       lambda { @client.get!("/test/413") }.should raise_exception ::Restfulie::Client::HTTP::Error::ClientError
     end
 
    it "raise Error::NotImplemented error when 501 code is returned" do
-      @client.get("/test/501")[2].code.should == 501
+      @client.get("/test/501").should respond_with_status(501)
       lambda { @client.get!("/test/501") }.should raise_exception ::Restfulie::Client::HTTP::Error::NotImplemented
     end
 
    it "raise Error::ServerError error when 500 code is returned" do
-      @client.get("/test/500")[2].code.should == 500
+      @client.get("/test/500").should respond_with_status(500)
       lambda { @client.get!("/test/500") }.should raise_exception ::Restfulie::Client::HTTP::Error::ServerError
     end
 
    it "raise Error::ServerNotAvailableError error when 503 code is returned" do
-      @client.get(nil,nil)[2].code.should == 503
+      @client.get(nil,nil).should respond_with_status(503)
       lambda { @client.get!(nil,nil) }.should raise_exception ::Restfulie::Client::HTTP::Error::ServerNotAvailableError
     end
 
    it "raise Error::ServerError error when 502..599 code is returned" do
-     @client.get("/test/502")[2].code.should == 502
+     @client.get("/test/502").should respond_with_status(502)
       lambda { @client.get!("/test/502") }.should raise_exception ::Restfulie::Client::HTTP::Error::ServerError
     end
 
    it "raise Error::UnknownError error when 600 or bigger code is returned" do
-     @client.get("/test/600")[2].code.should == 600
+     @client.get("/test/600").should respond_with_status(600)
       lambda { @client.get!("/test/600") }.should raise_exception ::Restfulie::Client::HTTP::Error::UnknownError
     end
 
--- spec/units/spec_helper.rb
+++ spec/units/spec_helper.rb
@@ -44,3 +44,36 @@ class AtomifiedModel
     entry
   end
 end
+
+module ResponseMatchers
+
+  class ResponseStatus
+
+    def initialize(response_code)
+      @expected_code = response_code.to_i
+    end
+
+    def matches?(response_header)
+      @actual_code = response_header[2].code
+      @expected_code == @actual_code
+    end
+
+    def failure_message
+        "expected response status to be #{@expected_code} but it's #{@actual_code}"
+    end
+
+    def negative_failure_message
+        "didn't expect #{@expected_code} to be #{@actual_code}"
+    end
+
+  end
+
+  def respond_with_status(response_code)
+    ResponseStatus.new(response_code)
+  end
+
+end
+
+Spec::Runner.configure do |config|
+   config.include(ResponseMatchers)
+end
