d3d57504e47e65c128c26cbc857ac4b4cbc68dd8
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-08-10 17:02:12 -0300

    they died. ghost methods

--- lib/restfulie/server/action_view/template_handlers.rb
+++ lib/restfulie/server/action_view/template_handlers.rb
@@ -4,18 +4,22 @@ module Restfulie
       module TemplateHandlers #:nodoc:
         autoload :Tokamak, 'restfulie/server/action_view/template_handlers/tokamak'
         
-        # It is needed to explicitly call 'activate!' to install the Tokamak
-        # template handler
-        def self.activate!
+        def self.template_registry
           if defined? ::ActionView::Template and 
             ::ActionView::Template.respond_to?(:register_template_handler)
               ::ActionView::Template
           else
             ::ActionView::Base
-          end.register_template_handler(:tokamak, 
+          end
+        end
+        
+        # It is needed to explicitly call 'activate!' to install the Tokamak
+        # template handler
+        def self.activate!
+          template_registry.register_template_handler(:tokamak, 
                 Restfulie::Server::ActionView::TemplateHandlers::Tokamak)
       
-          if defined? ::ActionController::Base
+          if defined?(::ActionController::Base) && ::ActionController::Base.respond_to?(:exempt_from_layout)
             ::ActionController::Base.exempt_from_layout :tokamak 
           end    
         end
