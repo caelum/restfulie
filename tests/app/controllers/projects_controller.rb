fcaa098c18e2ea52fdd6134f45120dd0f5986da9
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-08-17 14:38:50 -0300

    die

---
+++ tests/app/controllers/projects_controller.rb
@@ -0,0 +1,13 @@
+class ProjectsController < ApplicationController
+  self.responder = Restfulie::Server::ActionController::RestfulResponder
+
+  def index
+  end
+  
+  def show
+  end
+  
+  def new
+  end
+  
+end
