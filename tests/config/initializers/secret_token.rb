8dc2df9abd8151b0eb78f8c6a042d02b76bb1516
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-08-17 09:48:09 -0300

    adding test project

---
+++ tests/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tests::Application.config.secret_token = '84c6f1274c05f6f62d863693e6dea95462a61feeaf5d2f9845df2352f1d73bbffd729246f8603d2d200ae102a2e9adbe9d8c1b7120e8c2021252f287aadc2eb5'
