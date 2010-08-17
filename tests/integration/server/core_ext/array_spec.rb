c72c66c5253007828e97d79f1d2dc2e277dbdacc
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-08-17 10:31:04 -0300

    moving integration appart from unit

--- tests/integration/server/core_ext/array_spec.rb
+++ tests/integration/server/core_ext/array_spec.rb
@@ -3,13 +3,10 @@ require File.expand_path(File.dirname(__FILE__) + '/../../lib/models')
 
 context Array do
 
-  before(:each) do
-    @array = []
-    3.times { @array << AtomifiedModel.new }
-  end
-
   it "creates an Atom feed based on the array elements" do
-    feed = @array.to_atom
+    array = []
+    3.times { array << AtomifiedModel.new }
+    feed = array.to_atom
     feed = feed.to_xml
     feed.should include("<feed xmlns=\"http://www.w3.org/2005/Atom\"")
     feed.should include("<published>2010-05-03T16:29:26Z</published>")
@@ -19,7 +16,7 @@ context Array do
   
   it "accepts a block to customize fields" do
     
-    feed = @array.to_atom do |f|
+    feed = [].to_atom do |f|
       f.title = "customized title"
       f.authors << Restfulie::Common::Representation::Atom::Person.new("author", :name => 'John Doe')
     end.to_xml
@@ -29,15 +26,4 @@ context Array do
     feed.should include("<name>John Doe</name")
   end
   
-  it "get the max time to update the items" do
-    Album.create(:title => 'Array test')
-    Album.update_all(:updated_at => Time.now - 1.day)
-    Album.first.update_attributes(:updated_at => Time.now)
-    Album.all.updated_at.should == Album.first.updated_at
-  end
-  
-  it "custom field usinged to find max updated" do
-    album = Album.create(:title => 'Array test')
-    Album.all.updated_at(:created_at).to_i.should == album.created_at.to_i
-  end
 end
