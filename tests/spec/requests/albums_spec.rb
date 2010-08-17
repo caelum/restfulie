909254e8803e6d2343e338a32a0850d5c4ca8ee0
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-08-17 15:10:27 -0300

    trash trash trash

--- tests/spec/requests/albums_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "Albums" do
-  describe "GET /albums" do
-    it "works! (now write some real specs)" do
-      get albums_path
-    end
-  end
-end
--- tests/spec/requests/songs_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "Songs" do
-  describe "GET /songs" do
-    it "works! (now write some real specs)" do
-      get songs_path
-    end
-  end
-end
