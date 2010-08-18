12020f71204257f2b764bebcb2afd658c192d9e7
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-08-18 15:06:29 -0300

    moved client integration http tests

--- tests/spec/requests/http/marshal_spec.rb
+++ tests/spec/requests/http/marshal_spec.rb
@@ -1,5 +1,4 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
-require File.expand_path(File.dirname(__FILE__) + '/../../lib/data_helper')
 
 context Restfulie::Client::HTTP::RequestMarshaller do
 
