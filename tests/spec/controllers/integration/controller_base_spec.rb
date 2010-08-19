da1cfb78c41057c4c78b09a7c17bc8dd72ed66b1
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-08-19 13:29:25 -0300

    creating rails2 and 3 strategy

---
+++ tests/spec/controllers/integration/controller_base_spec.rb
@@ -0,0 +1,56 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+require "action_controller"
+
+class BaseController < ::ActionController::Base
+  include Restfulie::Server::ActionController::Base
+
+  def index
+    render :atom => [AtomifiedModel.new, AtomifiedModel.new, AtomifiedModel.new]
+  end
+  
+  def show
+    render :atom => AtomifiedModel.new
+  end
+  
+  def show_with_mock
+    render :atom => object
+  end
+end
+
+ActionController::Routing::Routes.draw do |map|
+  map.connect ':controller/:action/:id'
+end
+
+describe Restfulie::Server::ActionController::Base, :type => :controller do
+  tests BaseController
+  
+  before(:each) do
+    @object = mock("ModelWithToAtom")
+    @controller.stub!(:object).and_return(@object)
+  end
+  
+  it "renders show action with application/atom+xml" do
+    get :show, :id => 1
+    @response.content_type.should == "application/atom+xml"
+  end
+  
+  it "renders index action with application/atom+xml" do
+    get :index
+    @response.content_type.should == "application/atom+xml"
+  end
+  
+  it "calls to_atom in show's object if object respond_to :to_atom" do
+    @atom = Object.new
+    @object.should_receive(:to_atom).once.and_return(@atom)
+    @atom.should_receive(:to_xml).once
+    get :show_with_mock, :id => 1
+    @response.content_type.should == "application/atom+xml"
+  end
+
+  it "calls to_xml in show's object if object does not respond_to :to_atom" do
+    @object.should_receive(:to_xml).once
+    get :show_with_mock, :id => 1
+    @response.content_type.should == "application/atom+xml"    
+  end
+  
+end
