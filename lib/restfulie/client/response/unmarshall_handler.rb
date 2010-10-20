5072501a7ce6e0b26b6b2212db98f8d369374c99
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-10-20 13:10:18 -0200

    fixing paths

---
+++ lib/restfulie/client/response/unmarshall_handler.rb
@@ -0,0 +1,33 @@
+module Restfulie
+  module Client
+    module Response
+      class UnmarshallHandler
+  
+        def initialize(config, requester)
+          @config = config
+          @requester = requester
+        end
+  
+        # parses the http response.
+        # first checks if its a 201, redirecting to the resource location.
+        # otherwise check if its a raw request, returning the content itself.
+        # finally, tries to parse the content with a mediatype handler or returns the response itself.
+        def parse(host, path, http_request, request, response)
+          response = @requester.parse(host, path, http_request, request, response)
+          if response.code == 201
+            request = Restfulie.at(response.headers['location'])
+            request.accepts(@config.acceptable_mediatypes) if @config.acceptable_mediatypes
+            request.get!
+          elsif @config.raw?
+            response
+          elsif (!response.body.nil?) && !response.body.empty?
+            representation = Restfulie::Client::HTTP::RequestMarshaller.content_type_for(response.headers['content-type']) || Restfulie::Common::Representation::Generic.new
+            representation.unmarshal(response.body)
+          else
+            response
+          end
+        end
+      end
+    end
+  end
+end
