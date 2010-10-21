5760b69e88ac373b9cb40b2a4482f2fb6af7e1fa
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-10-21 00:05:25 -0200

    moving client unit tests

--- spec/unit/client/configuration_spec.rb
+++ spec/unit/client/configuration_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 module Restfulie::Client::Configuration::Test
   class AtomRepresentation; end
