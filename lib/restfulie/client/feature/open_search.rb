5050208eb2bd0cd97c61a9059baf985f77bfcbd5
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-10-23 01:52:24 -0200

    parsing parameters and testing them

--- lib/restfulie/client/feature/open_search.rb
+++ lib/restfulie/client/feature/open_search.rb
@@ -1,9 +1,11 @@
 module Restfulie::Client::Feature::OpenSearch
   
   def search(params)
-    get PatternMatcher.new.match(params, @params_pattern)
+    get PatternMatcher.new.match(params, params_pattern)
   end
   
+  attr_reader :params_pattern
+
   def with_pattern(params_pattern)
     @params_pattern = params_pattern
     self
--- spec/unit/common/representation/opensearch_spec.rb
+++ spec/unit/common/representation/opensearch_spec.rb
@@ -2,22 +2,26 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe Restfulie::Common::Representation::OpenSearch do
   
-  it "should unmarshall opensearch xml descriptions" do
-    xml = '<?xml version="1.0" encoding="UTF-8"?>
-    <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
-      <ShortName>Restbuy</ShortName>
-      <Description>Restbuy search engine.</Description>
-      <Tags>restbuy</Tags>
-      <Contact>admin@restbuy.com</Contact>
-      <Url type="application/atom+xml"  template="http://localhost:3000/products?q={searchTerms}&amp;pw={startPage?}&amp;format=atom" />
-    </OpenSearchDescription>'
-    descriptor = Restfulie::Common::Representation::OpenSearch.new.unmarshal(xml)
-    descriptor.urls.size.should == 1
-    descriptor.use("application/atom+xml").host.should == URI.parse("http://localhost:3000/products")
-    
-    # Restfulie.at("http://localhost:3000/search.xml").get.resource.accepts("application/atom+xml").search(:searchTerms => "apple", :startPage => 2)
-    
-    
-  end
+  context "unmarshalling one url documents" do
   
+    before do
+      xml = '<?xml version="1.0" encoding="UTF-8"?>
+      <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
+        <ShortName>Restbuy</ShortName>
+        <Description>Restbuy search engine.</Description>
+        <Tags>restbuy</Tags>
+        <Contact>admin@restbuy.com</Contact>
+        <Url type="application/atom+xml"  template="http://localhost:3000/products?q={searchTerms}&amp;pw={startPage?}&amp;format=atom" />
+      </OpenSearchDescription>'
+      @descriptor = Restfulie::Common::Representation::OpenSearch.new.unmarshal(xml)
+    end
+  
+    it "should unmarshall opensearch xml descriptions" do
+      @descriptor.urls.size.should == 1
+      @descriptor.use("application/atom+xml").host.should == URI.parse("http://localhost:3000/products")
+      @descriptor.use("application/atom+xml").params_pattern.should == "q={searchTerms}&pw={startPage?}&format=atom"
+    end
+  
+  end
+
 end
