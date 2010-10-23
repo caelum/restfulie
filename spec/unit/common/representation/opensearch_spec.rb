f8cb851b1803d5b59d1e9f55d1af02ecfd83cbe3
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-10-23 01:53:15 -0200

    testing the descriptor

--- spec/unit/common/representation/opensearch_spec.rb
+++ spec/unit/common/representation/opensearch_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
-describe Restfulie::Common::Representation::OpenSearch do
+describe Restfulie::Common::Representation::OpenSearch::Descriptor do
   
   context "unmarshalling one url documents" do
   
