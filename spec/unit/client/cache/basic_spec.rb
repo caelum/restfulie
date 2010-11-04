7c5867d35b9851d94b419a2e9dce6fc5a0fed036
guilherme silveira & pedro mariano & gabriel oliveira <guilherme.silveira@caelum.com.br>
2010-11-04 20:01:29 -0200

    fixing responder tests

--- spec/unit/client/cache/basic_spec.rb
+++ spec/unit/client/cache/basic_spec.rb
@@ -4,26 +4,36 @@ context Restfulie::Client::Cache::Basic do
   
   before do
     @response = Net::HTTPResponse
+    def @response.verb
+      :get
+    end
+  end
+  
+  def prepare_request
+    request = Object.new
+    def request.verb
+      :get
+    end
+    request
   end
   
   it "should put on the cache if Cache-Control is enabled" do
     url = Object.new
-    request = Object.new
+    request = prepare_request
     cache = Restfulie::Client::Cache::Basic.new
     @response.should_receive(:vary_headers_for).with(request).and_return(1)
     @response.should_receive(:vary_headers_for).with(request).and_return(1)
-    @response.should_receive(:method).and_return(:get)
     
     Restfulie::Client::Cache::Restrictions.should_receive(:may_cache?).with(@response).and_return(true)
     
     cache.put(url, request, @response)
     @response.should_receive(:has_expired_cache?).and_return(false)
-    cache.get(url, request, :get).should == @response
+    cache.get(url, request).should == @response
   end
 
   it "should respect the vary header" do
     url = Object.new
-    request = Object.new
+    request = prepare_request
     cache = Restfulie::Client::Cache::Basic.new
     @response.should_receive(:vary_headers_for).with(request).and_return(1)
     @response.should_receive(:vary_headers_for).with(request).and_return(2)
@@ -31,45 +41,44 @@ context Restfulie::Client::Cache::Basic do
     Restfulie::Client::Cache::Restrictions.should_receive(:may_cache?).with(@response).and_return(true)
     
     cache.put(url, request, @response)
-    cache.get(url, request, :get).should be_nil
+    cache.get(url, request).should be_nil
   end
 
   it "should not put on the cache if Cache-Control is enabled" do
     url = Object.new
-    request = Object.new
+    request = prepare_request
     cache = Restfulie::Client::Cache::Basic.new
     
     Restfulie::Client::Cache::Restrictions.should_receive(:may_cache?).with(@response).and_return(false)
     
     cache.put(url, request, @response)
-    cache.get(url, request, :get).should be_nil
+    cache.get(url, request).should be_nil
   end
 
   it "should remove from cache if expired" do
     url = Object.new
-    request = Object.new
+    request = prepare_request
     cache = Restfulie::Client::Cache::Basic.new
     @response.should_receive(:vary_headers_for).with(request).and_return(1)
     @response.should_receive(:vary_headers_for).with(request).and_return(1)
-    @response.stub(:method).and_return(:get)
     
     Restfulie::Client::Cache::Restrictions.should_receive(:may_cache?).with(@response).and_return(true)
     
     cache.put(url, request, @response)
     @response.should_receive(:has_expired_cache?).and_return(true)
-    cache.get(url, request, :get).should be_nil
+    cache.get(url, request).should be_nil
   end
 
   it "should allow cache clean up" do
     url = Object.new
-    request = Object.new
+    request = prepare_request
     cache = Restfulie::Client::Cache::Basic.new
     
     Restfulie::Client::Cache::Restrictions.should_receive(:may_cache?).with(@response).and_return(true)
     
     cache.put(url, request, @response)
     cache.clear
-    cache.get(url, request, :get).should be_nil
+    cache.get(url, request).should be_nil
   end
 
 end
