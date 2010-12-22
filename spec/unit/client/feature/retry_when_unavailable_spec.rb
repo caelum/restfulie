52a5466306aff2f3986adc7ec71d07b0a9862790
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2010-12-22 16:53:29 -0200

    fixing some 4 to 3 parameter refactor

--- spec/unit/client/feature/retry_when_unavailable_spec.rb
+++ spec/unit/client/feature/retry_when_unavailable_spec.rb
@@ -14,14 +14,14 @@ describe Restfulie::Client::Feature::RetryWhenUnavailable do
     
     it "should retry execution by default if response code is 503" do
       second_response = mock Net::HTTPResponse
-      @chain.should_receive(:continue).with(@request, nil, {}).and_return(second_response)
+      @chain.should_receive(:continue).with(@request, {}).and_return(second_response)
       @response.stub(:code).and_return("503")
-      @feature.execute(@chain, @request, nil, {}).should == second_response
+      @feature.execute(@chain, @request, {}).should == second_response
     end
     
     it "should not retry execution by default if response code is not 503" do
       @response.stub(:code).and_return("404")
-      @feature.execute(@chain, @request, nil, {}).should == @response
+      @feature.execute(@chain, @request, {}).should == @response
     end
     
     it "should retry execution with a custom behavior" do
@@ -30,7 +30,7 @@ describe Restfulie::Client::Feature::RetryWhenUnavailable do
       end
       second_response = mock Net::HTTPResponse
       @chain.should_receive(:continue).and_return(second_response)
-      @feature.execute(@chain, @request, nil, {"force_retry" => true}).should == second_response
+      @feature.execute(@chain, @request, {"force_retry" => true}).should == second_response
     end
     
   end
