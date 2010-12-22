b5205656701f4f2aefd0b1bbe460a83370773666
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2010-12-22 17:31:58 -0200

    version

--- lib/restfulie/version.rb
+++ lib/restfulie/version.rb
@@ -4,7 +4,7 @@ module Restfulie
     MINOR = 0
     TINY  = 0
 
-    STRING = [MAJOR, MINOR, TINY].join('.') + ".beta1"
+    STRING = [MAJOR, MINOR, TINY].join('.') + ".beta2"
 
     # Restfulie's version
     def self.to_s
