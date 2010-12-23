c873dc343e5baf791868c24099cc8088e8e2aa36
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2010-12-23 12:28:11 -0200

    removing tokamak xml to another gem

--- lib/restfulie/common/converter.rb
+++ lib/restfulie/common/converter.rb
@@ -10,17 +10,6 @@ module Restfulie
     module Converter
       Dir["#{File.dirname(__FILE__)}/converter/*.rb"].each {|f| autoload File.basename(f)[0..-4].camelize.to_sym, f }
 
-      # Returns the default root element name for an item or collection
-      def self.root_element_for(obj)
-        if obj.kind_of?(Hash) && obj.size==1
-          obj.keys.first.to_s
-        elsif obj.kind_of?(Array) && !obj.empty?
-          root_element_for(obj.first).to_s.underscore.pluralize
-        else
-          obj.class.to_s.underscore
-        end
-      end
-      
       def self.register(media_type,representation)
         representations[media_type] = representation
       end
