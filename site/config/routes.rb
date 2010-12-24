68e69cab3f6bd30230d4ccb3319f1830e173967b
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2010-12-24 01:39:22 -0200

    creating basic example doc

--- site/config/routes.rb
+++ site/config/routes.rb
@@ -5,7 +5,7 @@ ActionController::Routing::Routes.draw do |map|
   map.support '/support', :controller => :systems, :action => :support
   map.rest '/what_is_rest_and_hateoas', :controller => :systems, :action => :rest
   map.java '/restful_java', :controller => :restful_java, :action => :installing
-  # map.rails '/restful_rails', :controller => :restful_rails, :action => :installing
+  map.rails '/restful_rails', :controller => :restful_rails, :action => :index
   map.csharp '/restful_csharp', :controller => :restful_csharp, :action => :installing
   map.features '/restfulie_features', :controller => :systems, :action => :restfulie_features
   
