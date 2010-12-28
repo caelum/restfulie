dcd61ebd73657c333f29fb7d68730ea80b418da0
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2010-12-28 21:46:22 -0200

    replacing medie in common

--- lib/restfulie/common.rb
+++ lib/restfulie/common.rb
@@ -2,18 +2,15 @@ require 'net/http'
 require 'uri'
 
 require 'rubygems'
-require 'active_support'
-require 'action_controller'
 require 'tokamak'
+require 'medie'
 
 module Restfulie
   
   # Code common to both client and server side is contained in the common module.
   module Common
     autoload :Error, 'restfulie/common/error'
-    autoload :Links, 'restfulie/common/links'
     autoload :Logger, 'restfulie/common/logger'
-    autoload :Converter, 'restfulie/common/converter'
   end
 
   def self.extract_link_header(links)
@@ -30,5 +27,3 @@ module Medie
     end
   end
 end
-
-require 'restfulie/common/core_ext'
