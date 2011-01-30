675a189325d2cfb575ee42fdaf88ee7bb1f0bf13
Jose Donizetti <jdbjunior@gmail.com>
2011-01-30 18:37:00 -0200

    organizing actions

--- site/app/controllers/java_controller.rb
+++ site/app/controllers/java_controller.rb
@@ -1,5 +1,6 @@
 class JavaController < ApplicationController
   def index
+    render :action => 'basic'
   end
   
   def basic
@@ -14,35 +15,39 @@ class JavaController < ApplicationController
   def features
   end
   
-  def tokamak
-  end
-
-  def opensearch
+  def hypermedia
   end
   
-  #down here
-  def installing
-  end
-  def features
-  end
-  def short_samples
-  end
   def server
   end
+  
   def rendering_resources
   end
-  def server_state_machine
-  end
-  def client
-  end
-  def help_and_examples
-  end
-  def live_examples
-  end
-  def team
-  end
-  def roadmap
-  end
-  def what_is_new
-  end
+
+  
+  #down here
+  #def installing
+  #end
+  #def features
+  #end
+  #def short_samples
+  #end
+  #def server
+  #end
+  #def rendering_resources
+  #end
+  #def server_state_machine
+  #end
+  #def client
+  #end
+  #def help_and_examples
+  #end
+  #def live_examples
+  #end
+  #def team
+  #end
+  #def roadmap
+  #end
+  #def what_is_new
+  #end
 end
