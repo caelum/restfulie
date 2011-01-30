530c7450694316c28640ca1f25bb0d61fefba0c0
Jose Donizetti <jdbjunior@gmail.com>
2011-01-30 14:48:12 -0200

    view for java doc

---
+++ site/app/controllers/java_controller.rb
@@ -0,0 +1,48 @@
+class JavaController < ApplicationController
+  def index
+  end
+  
+  def basic
+  end
+  
+  def examples
+  end
+  
+  def conneg
+  end
+  
+  def features
+  end
+  
+  def tokamak
+  end
+
+  def opensearch
+  end
+  
+  #down here
+  def installing
+  end
+  def features
+  end
+  def short_samples
+  end
+  def server
+  end
+  def rendering_resources
+  end
+  def server_state_machine
+  end
+  def client
+  end
+  def help_and_examples
+  end
+  def live_examples
+  end
+  def team
+  end
+  def roadmap
+  end
+  def what_is_new
+  end
+end
