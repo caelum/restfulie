6c73a12254b9a8e26351af9029e4bd64c634c1af
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2011-02-07 15:13:28 -0200

    using last updated to fix failing cache tests

--- tests/app/controllers/cacheable_songs_controller.rb
+++ tests/app/controllers/cacheable_songs_controller.rb
@@ -4,20 +4,25 @@ class CacheableClientsController < ApplicationController
 
   def single
     response.last_modified = Time.utc(2010, 2) if params[:last_modified]
-    respond_with Song.new(:created_at => Time.utc(2010))
+    respond_with song_at(2010)
   end
 
   def collection
-    respond_with [Song.new(:created_at => Time.utc(2010)), Song.new(:created_at => Time.utc(2009))]
+    respond_with [song_at(2010), song_at(2009)]
   end
 
   def new_record
-    model = Song.new(:created_at => Time.utc(2010))
+    model = song_at(2010)
     model.new_record = true
     respond_with(model)
   end
 
   def empty
     respond_with []
+  end
+  
+  private
+  def song_at(year)
+    Song.new(:created_at => Time.utc(year), :updated_at => Time.utc(year))
   end
 end
