e0405a041000bbaa82628138e0c19867603d4f71
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2011-02-07 15:07:57 -0200

    using songs instead of clients

--- tests/app/controllers/cacheable_songs_controller.rb
+++ tests/app/controllers/cacheable_songs_controller.rb
@@ -1,19 +1,18 @@
 class CacheableClientsController < ApplicationController
   self.responder = Restfulie::Server::ActionController::RestfulResponder
-  # include Restfulie::Server::ActionController::Base
-  respond_to :atom
+  respond_to :json
 
   def single
     response.last_modified = Time.utc(2010, 2) if params[:last_modified]
-    respond_with AtomifiedModel.new(Time.utc(2010))
+    respond_with Song.new(:created_at => Time.utc(2010))
   end
 
   def collection
-    respond_with [AtomifiedModel.new(Time.utc(2010)), AtomifiedModel.new(Time.utc(2009))]
+    respond_with [Song.new(:created_at => Time.utc(2010)), Song.new(:created_at => Time.utc(2009))]
   end
 
   def new_record
-    model = AtomifiedModel.new(Time.utc(2010))
+    model = Song.new(:created_at => Time.utc(2010))
     model.new_record = true
     respond_with(model)
   end
