e9d22355bfddbbf543f6faaf00315ad56c74a1c1
Roberto Faria <roberto@bright-sun.org>
2011-06-03 00:25:42 +0800

    Enable ssl for a uri with the 'https' scheme

--- lib/restfulie/client/feature/base_request.rb
+++ lib/restfulie/client/feature/base_request.rb
@@ -1,5 +1,5 @@
 class Restfulie::Client::Feature::BaseRequest
-  
+
   def execute(flow, request, env)
     request!(request.verb, request.host, request.path, request, flow, env)
   end
@@ -11,7 +11,7 @@ class Restfulie::Client::Feature::BaseRequest
   def request!(method, host, path, request, flow, env)
 
     ::Restfulie::Common::Logger.logger.info(request.http_to_s(method, path, [request.headers])) if ::Restfulie::Common::Logger.logger
-    http_request = get_connection_provider(host)
+    http_request = get_connection_provider(host, env)
 
     if env[:body]
       enhance http_request.send(method, path, env[:body], request.headers)
@@ -23,12 +23,19 @@ class Restfulie::Client::Feature::BaseRequest
 
   protected
 
-  def get_connection_provider(host)
-    @connection ||= ::Net::HTTP.new(host.host, host.port)
+  def get_connection_provider(uri, env)
+    @connection ||= create_connection(uri, env)
   end
 
   private
-  
+  def create_connection(uri, env)
+    result = ::Net::HTTP.new(uri.host, uri.port)
+    result.use_ssl = true if uri.scheme == 'https'
+    ssl_verify_mode = env[:ssl_verify_mode]
+    result.verify_mode = ssl_verify_mode if ssl_verify_mode
+    result
+  end
+
   def enhance(response)
     def response.code
       super.to_i
